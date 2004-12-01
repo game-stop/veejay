de713d4f94887404cb03cdccd78930f142e3228b
Niels Elburg <niels@dyne.org>
2004-12-01 11:26:26 +0000

    moved
    git-svn-id: svn://code.dyne.org/veejay/trunk@52 eb8d1916-c9e9-0310-b8de-cf0c9472ead5

---
+++ veejay-current/libOSC/libosc.h
@@ -0,0 +1,18 @@
+#ifndef _LIBOSC_H_
+#define _LIBOSC_H_
+
+#include <libOSC/OSC-common.h>
+#include <libOSC/OSC-timetag.h>
+#include <libOSC/OSC-address-space.h>
+#include <libOSC/OSC-dispatch.h>
+#include <libOSC/OSC-receive.h>
+#include <libOSC/OSC-callbacklist.h>
+#include <libOSC/OSC-drop.h>
+#include <libOSC/OSC-internal-messages.h>
+#include <libOSC/OSC-pattern-match.h>
+#include <libOSC/OSC-priority-queue.h>
+#include <libOSC/OSC-string-help.h>
+#include <libOSC/NetworkReturnAddress.h>
+#include <libOSC/NetworkUDP.h>
+
+#endif
