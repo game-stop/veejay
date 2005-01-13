9aa3cdebf4448675a012a2461d76d9583a7f404c
Niels Elburg <niels@dyne.org>
2005-01-13 12:13:15 +0000

    refactorized
    git-svn-id: svn://code.dyne.org/veejay/trunk@126 eb8d1916-c9e9-0310-b8de-cf0c9472ead5

--- veejay-current/veejay/vj-server.h
+++ veejay-current/veejay/vj-server.h
@@ -26,42 +26,35 @@
 #define VJ_PORT 3490
 #define VJ_MAX_CONNECTIONS 8
 
+
 typedef struct vj_server_t {
-    fd_set master;
-    fd_set current;
     struct sockaddr_in myself;
     struct sockaddr_in remote;
     int handle;
     int nr_of_connections;
-    int nr_of_links;
+	void **link;
+	fd_set	fds;
+	fd_set	wds;
+	int	nr_of_links;
 } vj_server;
 
 int vj_server_init();
 
-vj_server *vj_server_alloc(int port, int type);
-int vj_server_send(int link_id, const char *buf, int len);
-int vj_server_update(vj_server * vje);
+vj_server *vj_server_alloc(int port);
 
-void vj_server_shutdown(vj_server *vje, int type);
+int vj_server_update(vj_server * vje, int link_id);
 
-int vj_server_adv_poll(vj_server *vje,int *links);
+void vj_server_shutdown(vj_server *vje);
 
-void vj_server_list_clients();
-
-int vj_server_retrieve_msg(int link_id, char *dst);
-
-int vj_server_status_check(vj_server * vje);
-
-int vj_server_status_send(vj_server *vje, char *buf, int len);
-
-void vj_server_close_link(vj_server * vje, int link_id, int type);
+int vj_server_retrieve_msg(vj_server *vje, int link_id, char *dst);
 
 int vj_server_poll(vj_server * vje);
 
-int vj_server_must_send(); 
-
-int		vj_server_raw_send(int link_id, uint8_t *buf, int len);
+int vj_server_send(vj_server *vje, int link_id, uint8_t *buf, int len);
 
 int	vj_server_init(void);
 
+void	vj_server_close_connection( vj_server *vje, int link_id );
+
+int	vj_server_new_connection(vj_server *vje);
 #endif
