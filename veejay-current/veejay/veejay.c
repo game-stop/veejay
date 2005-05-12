85d6e990ec72f60466dd6fc3ce2616ac02a2a2ce
Niels Elburg <niels@dyne.org>
2005-05-12 23:36:36 +0000

    version bump
    git-svn-id: svn://code.dyne.org/veejay/trunk@311 eb8d1916-c9e9-0310-b8de-cf0c9472ead5

--- veejay-current/veejay/veejay.c
+++ veejay-current/veejay/veejay.c
@@ -516,7 +516,7 @@ static void check_command_line_options(int argc, char *argv[])
 static void print_license()
 {
 	veejay_msg(VEEJAY_MSG_INFO,
-	    "Veejay -<|Argyre|>- %s Copyright (C) Niels Elburg and others",VERSION);
+	    "Veejay -<|Wirtz|>- %s Copyright (C) Niels Elburg and others",VERSION);
 	veejay_msg(VEEJAY_MSG_INFO,
 	    "This software is subject to the GNU GENERAL PUBLIC LICENSE");
 
