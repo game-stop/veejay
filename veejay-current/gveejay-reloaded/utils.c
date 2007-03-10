e786c9560370f42f7c4a2c253c88266167ea187a
Niels Elburg <niels@dyne.org>
2007-03-10 20:04:16 +0000

    gveejay64
    git-svn-id: svn://code.dyne.org/veejay/trunk@829 eb8d1916-c9e9-0310-b8de-cf0c9472ead5

--- veejay-current/gveejay-reloaded/utils.c
+++ veejay-current/gveejay-reloaded/utils.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-
+#include <config.h>
 #include <stdio.h>
 #include <mjpegtools/mpegconsts.h>
 #include <mjpegtools/mpegtimecode.h>
