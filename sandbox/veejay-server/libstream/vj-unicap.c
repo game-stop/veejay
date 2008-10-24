f563d54ef4c247aefb69947747769cd2b8c41715
Niels Elburg <niels@dyne.org>
2008-10-24 21:41:23 +0000

    more mem
    git-svn-id: svn://code.dyne.org/veejay/trunk@1118 eb8d1916-c9e9-0310-b8de-cf0c9472ead5

--- sandbox/veejay-server/libstream/vj-unicap.c
+++ sandbox/veejay-server/libstream/vj-unicap.c
@@ -962,7 +962,7 @@ static int	vj_unicap_start_capture_( void *vut )
 		assert( v->frame_size > 0 );
 #endif
 		v->priv_buf = (uint8_t*) vj_calloc( v->dst_width * v->dst_height * 4 * sizeof(uint8_t) );
-		v->buffer.data = vj_malloc( (v->src_sizes[0] + v->src_sizes[1] + v->src_sizes[2]) * sizeof(uint8_t) );
+		v->buffer.data = vj_malloc( (v->src_sizes[0] + v->src_sizes[1] + v->src_sizes[2]) * sizeof(uint8_t) * 2 );
 		if(!v->rgb)
 		{
 			veejay_memset( v->buffer.data,    0, v->src_sizes[0] * sizeof(uint8_t));
