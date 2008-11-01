df26a2bb241bedd01b2ee1f3f8ac9997050cb301
Niels Elburg <niels@dyne.org>
2008-11-01 03:32:19 +0000

    move stuff
    git-svn-id: svn://code.dyne.org/veejay/trunk@1134 eb8d1916-c9e9-0310-b8de-cf0c9472ead5

--- veejay-utils/src/sequence.h
+++
@@ -1,8 +0,0 @@
-int	veejay_get_image( void *data , guchar *pixels);
-
-void	*veejay_sequence_init(int port, char *hostname, gint w, gint h);
-
-void	veejay_configure_sequence( void *data, gint w, gint h );
-
-void	veejay_sequence_free( void *data );
-
