edbdb2f3129b9b6920affe4bd496164d89888bb4
Niels Elburg <niels@dyne.org>
2008-11-02 01:03:27 +0000

    update email adres
    git-svn-id: svn://code.dyne.org/veejay/trunk@1140 eb8d1916-c9e9-0310-b8de-cf0c9472ead5

--- veejay-1.3/veejay-server/libsample/sampleadm.c
+++ veejay-1.3/veejay-server/libsample/sampleadm.c
@@ -1,7 +1,7 @@
 /* 
  * Linux VeeJay
  *
- * Copyright(C)2002 Niels Elburg <elburg@hio.hen.nl> / <nelburg@looze.net>
+ * Copyright(C)2002 Niels Elburg <elburg@hio.hen.nl> / <nwelburg@gmail.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
