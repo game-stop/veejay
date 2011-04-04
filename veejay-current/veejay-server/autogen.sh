be4c9b729b796bfee3174e7e9154d0b1b0d2cb9f
niels <niels@dyne.org>
2011-04-04 19:26:53 +0200

    build fixes

--- veejay-current/veejay-server/autogen.sh
+++ veejay-current/veejay-server/autogen.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
+# if you use autoconf 2.64 or earlier,
+# you may have to create the m4 directory yourself
+# 
 
-autoreconf -v -f -i
+autoreconf -v -fi -I "${ORIGDIR}/m4"
