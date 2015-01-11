ffdfae2b98b603605467ac451644bf4d06f0e47b
niels <niels@dyne.org>
2015-01-11 18:53:49 +0100

    add LVD Color Tone FX

--- veejay-current/veejay-server/livido-plugins/lvd_colortone.c
+++ veejay-current/veejay-server/livido-plugins/lvd_colortone.c
@@ -1,20 +1,8 @@
 /* 
  * LIBVJE - veejay fx library
  *
- * Copyright(C)2011 Niels Elburg <nwelburg@gmail.com>
+ * Copyright(C)2015 Niels Elburg <nwelburg@gmail.com>
  * See COPYING for software license and distribution details
- */
-
-/*
- * lifecycle:
- *
- *   HOST calls livido_setup
- *   
- *   HOST calls init_instance *once* before calling process_instance
- *   HOST calls deinit_instance when plugin is no longer needed.
- *   HOST calls process_instance 
- *
- *
  */
 
 #ifndef IS_LIVIDO_PLUGIN
@@ -51,24 +39,29 @@ livido_process_f		process_instance( livido_port_t *my_instance, double timecode 
 	int w;
 	int h;
 	
-	//@ get output channel details
 	int error	  = lvd_extract_channel_values( my_instance, "out_channels", 0, &w,&h, O,&palette );
 	if( error != LIVIDO_NO_ERROR )
-		return LIVIDO_ERROR_HARDWARE; //@ error codes in livido flanky
+		return LIVIDO_ERROR_HARDWARE; 
+
+        lvd_extract_channel_values( my_instance, "in_channels" , 0, &w, &h, A, &palette );
 
 	int uv_len = lvd_uv_plane_len( palette,w,h );
 	len = w * h;
 
-	//@ get parameter values
-	int		r =  lvd_extract_param_index( my_instance,"in_parameters", 0 );
-	int		g =  lvd_extract_param_index( my_instance,"in_parameters", 1 );
-	int		b =  lvd_extract_param_index( my_instance,"in_parameters", 2 );
+	int		red    =  lvd_extract_param_index( my_instance,"in_parameters", 0 );
+	int		green  =  lvd_extract_param_index( my_instance,"in_parameters", 1 );
+	int		blue   =  lvd_extract_param_index( my_instance,"in_parameters", 2 );
+	int		shift = 1;
+        if( palette == LIVIDO_PALETTE_YUV444P )
+                shift = 0;
+	
+	if( O[0] != A[0] ) {
+		livido_memcpy( A[0], O[0], len );
+	}
+	
+	int		y,u,v;
+	GIMP_rgb2yuv( red, green, blue, y, u, v );
 
-	uint8_t y,u,v;
-
-	GIMP_rgb2yuv(r,g,b,y,u,v);
-
-	livido_memset( O[0], y, len );
 	livido_memset( O[1], u, uv_len );
 	livido_memset( O[2], v, uv_len );
 
@@ -104,9 +97,9 @@ livido_port_t	*livido_setup(livido_setup_t list[], int version)
 	port = filter;
 
 	//@ meta information
-		livido_set_string_value( port, "name", "Solid Color Fill");	
-		livido_set_string_value( port, "description", "Fill frame with a single color");
-		livido_set_string_value( port, "author", "Niels Elburg");
+		livido_set_string_value( port, "name", "Color tone");	
+		livido_set_string_value( port, "description", "Color tone");
+		livido_set_string_value( port, "author", "Niels Elburg"); //ported to livido
 		
 		livido_set_int_value( port, "flags", 0);
 		livido_set_string_value( port, "license", "GPL2");
@@ -114,18 +107,24 @@ livido_port_t	*livido_setup(livido_setup_t list[], int version)
 	
 	//@ some palettes veejay-classic uses
 	int palettes0[] = {
-           	LIVIDO_PALETTE_YUV420P,
-           	LIVIDO_PALETTE_YUV422P,
-            0
+		LIVIDO_PALETTE_YUV422P,
+		LIVIDO_PALETTE_YUV444P,
+            	0,
 	};
 	
 	//@ setup output channel
 	out_chans[0] = livido_port_new( LIVIDO_PORT_TYPE_CHANNEL_TEMPLATE );
 	port = out_chans[0];
 	
-	    livido_set_string_value( port, "name", "Output Channel");
+		livido_set_string_value( port, "name", "Output Channel");
 		livido_set_int_array( port, "palette_list", 3, palettes0);
 		livido_set_int_value( port, "flags", 0);
+
+        in_chans[0] = livido_port_new( LIVIDO_PORT_TYPE_CHANNEL_TEMPLATE );
+        port = in_chans[0];
+  		livido_set_string_value( port, "name", "Input Channel");
+       		livido_set_int_array( port, "palette_list", 4, palettes0);
+       		livido_set_int_value( port, "flags", 0);
 	
 	//@ setup parameters (INDEX type, 0-255) 
 	in_params[0] = livido_port_new( LIVIDO_PORT_TYPE_PARAMETER_TEMPLATE );
@@ -135,37 +134,37 @@ livido_port_t	*livido_setup(livido_setup_t list[], int version)
 		livido_set_string_value(port, "kind", "INDEX" );
 		livido_set_int_value( port, "min", 0 );
 		livido_set_int_value( port, "max", 255 );
-		livido_set_int_value( port, "default", 0 );
+		livido_set_int_value( port, "default", 112 );
 		livido_set_string_value( port, "description" ,"Color Red");
 
 	
 	in_params[1] = livido_port_new( LIVIDO_PORT_TYPE_PARAMETER_TEMPLATE );
 	port = in_params[1];
 
-		livido_set_string_value(port, "name", "Blue" );
+		livido_set_string_value(port, "name", "Green" );
 		livido_set_string_value(port, "kind", "INDEX" );
 		livido_set_int_value( port, "min", 0 );
 		livido_set_int_value( port, "max", 255 );
-		livido_set_int_value( port, "default", 0 );
-		livido_set_string_value( port, "description" ,"Color Blue");
+		livido_set_int_value( port, "default", 66 );
+		livido_set_string_value( port, "description" ,"Color Green");
 
 
 	
 	in_params[2] = livido_port_new( LIVIDO_PORT_TYPE_PARAMETER_TEMPLATE );
 	port = in_params[2];
 
-		livido_set_string_value(port, "name", "Green" );
+		livido_set_string_value(port, "name", "Blue" );
 		livido_set_string_value(port, "kind", "INDEX" );
-		livido_set_int_value( port, "min", 0 );
+		livido_set_int_value( port, "min", 0);
 		livido_set_int_value( port, "max", 255 );
-		livido_set_int_value( port, "default", 0 );
-		livido_set_string_value( port, "description" ,"Color Green");
+		livido_set_int_value( port, "default", 20 );
+		livido_set_string_value( port, "description" ,"Speed");
 
 
 	//@ setup the nodes
 	livido_set_portptr_array( filter, "in_parameter_templates",3, in_params );
-	livido_set_portptr_array( filter, "in_channel_templates",0, NULL );
 	livido_set_portptr_array( filter, "out_channel_templates", 1, out_chans );
+        livido_set_portptr_array( filter, "in_channel_templates",1, in_chans );
 
 	livido_set_portptr_value(info, "filters", filter);
 	return info;
