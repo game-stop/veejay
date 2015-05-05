c60eba2ea1ac1829f26ec16cfddbb724ad760282
niels <niels@dyne.org>
2015-05-05 23:11:42 +0200

    fix picture in picture effect

--- veejay-current/veejay-server/libvje/effects/picinpic.c
+++ veejay-current/veejay-server/libvje/effects/picinpic.c
@@ -28,12 +28,13 @@
 #include <libvjmem/vjmem.h>
 #include "picinpic.h"
 #include <libyuv/yuvconv.h>
+#include <libavutil/pixfmt.h>
 #include "common.h" 
 extern void    vj_get_yuv444_template(VJFrame *src, int w, int h);
 typedef struct
 {
 	void		*scaler;
-	VJFrame		frame;
+	VJFrame		*frame;
 	sws_template	template;
 	void		*sampler;
 	int		cached;	
@@ -50,19 +51,19 @@ vj_effect *picinpic_init(int width, int height)
     ve->defaults = (int *) vj_calloc(sizeof(int) * ve->num_params);	/* default values */
     ve->limits[0] = (int *) vj_calloc(sizeof(int) * ve->num_params);	/* min */
     ve->limits[1] = (int *) vj_calloc(sizeof(int) * ve->num_params);	/* max */
-    ve->defaults[0] = 64;	/* width of view port */
-    ve->defaults[1] = 64;	/* height of viewport */
-    ve->defaults[2] = 64;	/* x1 */
-    ve->defaults[3] = 64;	/* y1 */
+    ve->defaults[0] = width/8;	/* width of view port */
+    ve->defaults[1] = height/8;	/* height of viewport */
+    ve->defaults[2] = width/2;	/* x1 */
+    ve->defaults[3] = height/2;	/* y1 */
 
     ve->limits[0][0] = 8;
-    ve->limits[1][0] = nearest_div(width);
+    ve->limits[1][0] = width;
     ve->limits[0][1] = 8;
-    ve->limits[1][1] = nearest_div(height);
-    ve->limits[0][2] = 8;
-    ve->limits[1][2] = nearest_div(width);
-    ve->limits[0][3] = 8;
-    ve->limits[1][3] = nearest_div(height);
+    ve->limits[1][1] = height;
+    ve->limits[0][2] = 0;
+    ve->limits[1][2] = width;
+    ve->limits[0][3] = 0;
+    ve->limits[1][3] = height;
 
     ve->description = "Picture in picture";
     ve->sub_format = 1;
@@ -86,7 +87,7 @@ int	picinpic_malloc(void **d, int w, int h)
 	my->template.flags = 1;
 	my->w = 0;
 	my->h = 0;
-
+	my->frame = NULL;
 	return 1;	
 }
 
@@ -102,12 +103,6 @@ void picinpic_apply( void *user_data, VJFrame *frame, VJFrame *frame2, int width
 		   int twidth, int theight, int x1, int y1 ) 
 {
 	int x, y;
-	uint8_t *dY = frame->data[0];
-	uint8_t *dCb = frame->data[1];
-	uint8_t *dCr = frame->data[2];
-	uint8_t *sY = frame2->data[0];
-	uint8_t *sCb = frame2->data[1];
-	uint8_t *sCr = frame2->data[2];
 	pic_t	*picture = (pic_t*) user_data;
 	int view_width = nearest_div(twidth);
 	int view_height = nearest_div(theight);
@@ -125,59 +120,71 @@ void picinpic_apply( void *user_data, VJFrame *frame, VJFrame *frame2, int width
 	if(view_width < 8 || view_height < 1  )
 		return; // nothing to do
 
-	/* pic in pic, using 444p */
-	VJFrame scale_src;	
-	vj_get_yuv444_template( &(scale_src), width,height );
+	// sub_format up is always 4:4:4 planar
+	int pixfmt = (frame->format == PIX_FMT_YUVJ422P ? PIX_FMT_YUVJ444P: PIX_FMT_YUV444P);
+	VJFrame src;
+	veejay_memcpy(&src,frame2,sizeof(VJFrame));
+	src.format = pixfmt;
+	src.stride[1] = src.width;
+	src.stride[2] = src.width;
 
-	scale_src.data[0] = frame2->data[0];
-	scale_src.data[1] = frame2->data[1];	
-	scale_src.data[2] = frame2->data[2]; 
-
-	/* Setup preview scaler */	
 	if( picture->w != view_width || picture->h != view_height || picture->w == 0 || picture->h == 0)
 	{
-		int len = (view_width * view_height);
-	
-		if(picture->scaler)
+		if(picture->frame) {
+			for( x = 0; x < 3; x ++ ) {
+				if(picture->frame->data[x])
+					free(picture->frame->data[x]);
+				picture->frame->data[x] = NULL;
+			}
+			free(picture->frame);
+			picture->frame = NULL;
+		}	
+		if( picture->scaler ) {
 			yuv_free_swscaler( picture->scaler );
-		if(picture->frame.data[0])
-			free( picture->frame.data[0] );
-		if(picture->frame.data[1])
-			free( picture->frame.data[1] );
-		if(picture->frame.data[2])
-			free( picture->frame.data[2] );
-		/* Allocate in picture */
-		vj_get_yuv444_template( &(picture->frame), view_width,view_height );
+			picture->scaler = NULL;
+		}
 
-		picture->scaler = yuv_init_swscaler(
-			&(scale_src),
-			&(picture->frame),
-			&(picture->template),
-			yuv_sws_get_cpu_flags()
-		);
-		picture->frame.data[0] = (uint8_t*) vj_calloc(sizeof(uint8_t) * RUP8(len) );
-		picture->frame.data[1] = (uint8_t*) vj_malloc(sizeof(uint8_t) * RUP8(len) );
-		picture->frame.data[2] = (uint8_t*) vj_malloc(sizeof(uint8_t) * RUP8(len) );
-		veejay_memset( picture->frame.data[1],128, len );
-		veejay_memset( picture->frame.data[2],128, len );
 		picture->w = view_width;
 		picture->h = view_height;
-
 	}
 
-	yuv_convert_and_scale( picture->scaler, &(scale_src), &(picture->frame) ); 
+	if( picture->frame == NULL ) {
+		picture->frame = yuv_yuv_template( NULL,NULL,NULL ,view_width,view_height,pixfmt );
+		// needs seperately allocated planes
+		picture->frame->data[0] = (uint8_t*) vj_malloc(sizeof(uint8_t) * RUP8( view_width * view_height ));
+		picture->frame->data[1] = (uint8_t*) vj_malloc(sizeof(uint8_t) * RUP8( view_width * view_height ));
+		picture->frame->data[2] = (uint8_t*) vj_malloc(sizeof(uint8_t) * RUP8( view_width * view_height ));
+	}
 
+	if( picture->scaler == NULL ) {
+		picture->scaler = yuv_init_swscaler(
+			&src,
+			picture->frame,
+			&(picture->template),
+			yuv_sws_get_cpu_flags() );
+		if(picture->scaler == NULL ) {
+			return;
+		}
+	}
+
+	yuv_convert_and_scale( picture->scaler, &src, picture->frame );
+
+	uint8_t *sY = picture->frame->data[0];
+	uint8_t *sCb = picture->frame->data[1];
+	uint8_t *sCr = picture->frame->data[2];
+
+	uint8_t *dY = frame->data[0];
+	uint8_t *dCb = frame->data[1];
+	uint8_t *dCr = frame->data[2]; 
+	
 	/* Copy the scaled image to output */
 	for( y = 0 ; y < picture->h-1; y ++ )
 	{
 		for( x = 0 ; x < picture->w-1; x ++ )
 		{
-			dY[ (dy + y ) * width + dx + x ] = 
-				picture->frame.data[0][ y * picture->w + x];
-			dCb[(dy + y ) * width + dx + x ] = 
-				picture->frame.data[1][ y * picture->w + x];
-			dCr[ (dy + y ) * width + dx + x ] = 
-				picture->frame.data[2][ y * picture->w + x];
+			dY[ (dy + y ) * width + dx + x ] =  sY[ y * picture->w + x];
+			dCb[(dy + y ) * width + dx + x ] =  sCb[ y * picture->w + x];
+			dCr[ (dy + y ) * width + dx + x ] = sCr[ y * picture->w + x];
 		}
 	}
 
@@ -191,13 +198,16 @@ void picinpic_free(void *d)
 		if(my->scaler)
 			yuv_free_swscaler( my->scaler );
 		int i;
-		for( i = 0; i < 3; i ++ ) {
-			if( my->frame.data[i] ) {
-				free(my->frame.data[i]);
+
+		if(my->frame) {
+			for( i = 0; i < 3; i ++ ) {
+				if(my->frame->data[i])
+					free(my->frame->data[i]);
+				my->frame->data[i] = NULL;
 			}
-			my->frame.data[i] = NULL;
+			free(my->frame);
 		}
-		
+
 		free( my );
 	}
 	d=NULL;
