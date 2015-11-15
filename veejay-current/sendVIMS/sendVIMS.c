de7563887ad04da31c3d7f4510befaaf97b58b44
niels <niels@dyne.org>
2015-11-15 13:29:35 +0100

    update number of symbols in sendVIMS

--- veejay-current/sendVIMS/sendVIMS.c
+++ veejay-current/sendVIMS/sendVIMS.c
@@ -159,16 +159,16 @@ pd_msg_t *pd_msg_new(char *msg){
     int n = 0;
 	
     /* get 26 ints */
-    n = sscanf(body, "%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d",
+    n = sscanf(body, "%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d",
 		s+0, s+1, s+2, s+3,
 		s+4, s+5, s+6, s+7,
 		s+8, s+9, s+10, s+11,
 		s+12,s+13,s+14, s+15,
 		s+16,s+17,s+18, s+19,
 		s+20,s+21,s+22, s+23,
-	       	s+24,s+25);
+	       	s+24,s+25, s+26);
 
-    if( n != 26 )
+    if( n != 27 )
 	    goto error;
 
     /* create msg */
