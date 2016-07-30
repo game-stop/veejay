9d6cd227e78acc3385fd4d8c5563b0068e37a443
[d.j.a.y] Jerome Blanchi <d.j.a.y@free.fr>
2016-07-30 11:16:01 +0200

    Libvje / refactor : chromamagicalpha
    * w,h
    * clean headers

--- veejay-current/veejay-server/libvje/vj-effman.c
+++ veejay-current/veejay-server/libvje/vj-effman.c
@@ -634,7 +634,7 @@ static void vj_effman_apply_video_effect( VJFrame **frames, vjp_kf *todo_info,in
 		lumakeyalpha_apply(frames[0],frames[1],arg[0],arg[1]);
 		break;
 	case VJ_VIDEO_EFFECT_CHROMAMAGICKALPHA:
-		chromamagickalpha_apply(frames[0],frames[1],frames[0]->width,frames[0]->height,arg[0],arg[1]);
+		chromamagickalpha_apply(frames[0],frames[1],arg[0],arg[1]);
 		break;
 	case VJ_VIDEO_EFFECT_MAGICOVERLAYALPHA:
 		overlaymagicalpha_apply(frames[0],frames[1],frames[0]->width,frames[0]->height,arg[0],arg[1]);
