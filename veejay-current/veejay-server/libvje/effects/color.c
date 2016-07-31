dc4e5b3f8c75dc6c74a48183f38798408463a8d7
[d.j.a.y] Jerome Blanchi <d.j.a.y@free.fr>
2016-07-31 14:27:27 +0200

    Libvje / refactor : color (fix)
    * clean headers

--- veejay-current/veejay-server/libvje/effects/color.c
+++ veejay-current/veejay-server/libvje/effects/color.c
@@ -17,9 +17,8 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307 , USA.
  */
-#include <stdint.h>
-#include <stdio.h>
-#include <libvje/vje.h>
+
+#include "common.h"
 #include <libvjmem/vjmem.h>
 #include "color.h"
 
