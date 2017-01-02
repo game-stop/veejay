a433c773347a990d27033f675fbb250d0fc5f8b7
c0ntrol <nwelburg@gmail.com>
2017-01-02 16:30:41 +0100

    removed min/max levels from rgbkey

--- veejay-current/veejay-server/libvje/vj-effman.c
+++ veejay-current/veejay-server/libvje/vj-effman.c
@@ -491,7 +491,7 @@ static void vj_effman_apply_video_effect( VJFrame **frames, vjp_kf *todo_info,in
 	break;
       case VJ_VIDEO_EFFECT_RGBKEY:
 	rgbkey_apply(frames[0], frames[1], arg[0], arg[1], arg[2], arg[3],
-	             arg[4], arg[5],arg[6],arg[7]);
+	             arg[4], arg[5]);
 	break;
       case VJ_VIDEO_EFFECT_KEYSELECT:
 	keyselect_apply(frames[0],frames[1],arg[0],arg[1],arg[2],arg[3],arg[4],arg[5]);
