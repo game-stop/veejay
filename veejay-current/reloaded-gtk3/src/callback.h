3fd6b101ce4c6bcb3a46debea18be35a2d716907
c0ntrol <nwelburg@gmail.com>
2019-05-26 23:08:07 +0200

    do not lock parameters in callback

--- veejay-current/reloaded-gtk3/src/callback.h
+++ veejay-current/reloaded-gtk3/src/callback.h
@@ -37,14 +37,12 @@ update_label_str( "value_friendlyname", _effect_get_hint( entry_tokens[ENTRY_FXI
 {\
 if(!info->status_lock && !info->parameter_lock)\
 {\
-info->parameter_lock = 1;\
 multi_vims( VIMS_CHAIN_ENTRY_SET_ARG_VAL, "%d %d %d %d", 0, info->uc.selected_chain_entry,arg_num, (get_slider_val(name) + fraction) );\
 update_slider_value( name, (get_slider_val(name) + fraction), 0 );\
 vj_midi_learning_vims_fx( info->midi, arg_num, VIMS_CHAIN_ENTRY_SET_ARG_VAL, 0, info->uc.selected_chain_entry,arg_num,2 );\
 if(info->uc.selected_rgbkey) update_rgbkey_from_slider(); \
 int *entry_tokens = &(info->uc.entry_tokens[0]);\
 update_label_str( "value_friendlyname", _effect_get_hint( entry_tokens[ENTRY_FXID], arg_num, get_slider_val(name) ));\
-info->parameter_lock = 0;\
 }\
 }
 
