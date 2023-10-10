0c791babdcbbccc48eda9614b2d2e3de8566b8b6
veejay <>
2023-10-10 03:43:19 +0200

    add new mode to halftone effect

--- veejay-current/veejay-server/libvje/effects/halftone.c
+++ veejay-current/veejay-server/libvje/effects/halftone.c
@@ -40,7 +40,7 @@ vj_effect *halftone_init(int w, int h)
     ve->limits[0][0] = 2;
     ve->limits[1][0] = ( w > h ? w / 2 : h / 2 );
     ve->limits[0][1] = 0;
-    ve->limits[1][1] = 3;
+    ve->limits[1][1] = 4;
     ve->limits[0][2] = 0;
     ve->limits[1][2] = 1;
     ve->limits[0][3] = 0;
@@ -58,12 +58,70 @@ vj_effect *halftone_init(int w, int h)
 
     ve->hints = vje_init_value_hint_list( ve->num_params );
 
-    vje_build_value_hint_list( ve->hints, ve->limits[1][1],1, "White Dots", "Black Dots", "Gray Dots", "Colored Dots" );
+    vje_build_value_hint_list( ve->hints, ve->limits[1][1],1, "White Dots", "Black Dots", "Gray Dots", "Colored Dots", "Averaged Colored Dots" );
     vje_build_value_hint_list( ve->hints, ve->limits[1][2],2, "Centered", "North West");// , "North", "North East", "East", "South East" ...); // TODO
     vje_build_value_hint_list( ve->hints, ve->limits[1][3],3, "Even", "Odd", "No parity"); //TODO add 'Berzek?' parameter aka broken/random parity; very cool on Mode animation
 
     return ve;
 }
+
+static void halftone_apply_avg_col2( VJFrame *frame, int radius, int orientation, int odd)
+{
+    uint8_t *Y = frame->data[0];
+    uint8_t *U = frame->data[1];
+    uint8_t *V = frame->data[2];
+    
+    const int w = frame->width;
+    const int h = frame->height;
+    const int rad = radius/2;
+    const int bw = radius;
+    const int bh = radius;
+
+    int x,y,x1,y1,x_inf,y_inf, x_sup, y_sup;
+
+    x_inf = 0; // initial init for North East
+    y_inf = 0;
+    x_sup = w;
+    y_sup = h;
+
+    grid_getbounds_from_orientation(radius, orientation, odd, &x_inf, &y_inf, &x_sup, &y_sup);
+
+    for( y = y_inf; y < h; y += radius ) {
+        for( x = x_inf; x < w; x += radius ) {
+            uint32_t sum = 0;
+	    int32_t  sumU = 0, sumV = 0;
+            uint32_t hit = 0;
+
+            // clip negative index (out of image) for colors pickup
+            for( y1 = (y < 0) ? 0 : y ; y1 < (y + radius) && y1 < h; y1 ++ ) {
+                for( x1 = (x < 0) ? 0 : x ; x1 < (x + radius) && x1 < w; x1 ++ ) {
+                    sum += Y[ y1 * w + x1 ];
+		    sumU += (U[ y1 * w + x1 ] - 128);
+		    sumV += (V[ y1 * w + x1 ] - 128);
+
+                    hit ++;
+                    
+		    Y[ y1 * w + x1 ] = pixel_Y_lo_;
+                    U[ y1 * w + x1 ] = 128;
+                    V[ y1 * w + x1 ] = 128;
+                }
+            }
+
+            uint32_t val = (sum / hit);
+            int wrad = 1 + (int) ( ((double) val / 255.0  ) * rad);
+             
+	    uint32_t valU = 128 + ( sumU / hit );
+	    uint32_t valV = 128 + ( sumV / hit );
+
+            veejay_draw_circle( Y , x,y, bw, bh, w, h, wrad, val );
+            veejay_draw_circle( U , x,y, bw, bh, w, h, wrad, valU );
+            veejay_draw_circle( V , x,y, bw, bh, w, h, wrad, valV );
+        }
+    }
+
+}
+
+
 
 static void halftone_apply_avg_col( VJFrame *frame, int radius, int orientation, int odd)
 {
@@ -300,5 +358,9 @@ void halftone_apply( void *ptr, VJFrame *frame, int *args ) {
         case 3:
             halftone_apply_avg_col( frame, radius , (vj_effect_orientation)orientation, (vj_effect_parity)parity);
             break;
+	case 4:
+            halftone_apply_avg_col2( frame, radius , (vj_effect_orientation)orientation, (vj_effect_parity)parity);
+            break;
+
     }
 }
