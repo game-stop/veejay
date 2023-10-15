6f7062c0d8be29ad1b2376cb61a717470e61c5dd
veejay <>
2023-10-15 20:43:09 +0200

    update blend by color key

--- veejay-current/veejay-server/libvje/effects/greyselect.c
+++ veejay-current/veejay-server/libvje/effects/greyselect.c
@@ -27,14 +27,14 @@ vj_effect *greyselect_init(int w, int h)
     vj_effect *ve;
     ve = (vj_effect *) vj_calloc(sizeof(vj_effect));
     ve->num_params = 5;
-    ve->defaults = (int *) vj_calloc(sizeof(int) * ve->num_params);	/* default values */
-    ve->limits[0] = (int *) vj_calloc(sizeof(int) * ve->num_params);	/* min */
-    ve->limits[1] = (int *) vj_calloc(sizeof(int) * ve->num_params);	/* max */
-    ve->defaults[0] = 4500;	/* angle */
-    ve->defaults[1] = 255;	/* r */
-    ve->defaults[2] = 0;	/* g */
-    ve->defaults[3] = 0;	/* b */
-	ve->defaults[4] = 0;	/* swap */
+    ve->defaults = (int *) vj_calloc(sizeof(int) * ve->num_params); /* default values */
+    ve->limits[0] = (int *) vj_calloc(sizeof(int) * ve->num_params);    /* min */
+    ve->limits[1] = (int *) vj_calloc(sizeof(int) * ve->num_params);    /* max */
+    ve->defaults[0] = 4500; /* angle */
+    ve->defaults[1] = 255;  /* r */
+    ve->defaults[2] = 0;    /* g */
+    ve->defaults[3] = 0;    /* b */
+    ve->defaults[4] = 0;    /* swap */
     ve->limits[0][0] = 1;
     ve->limits[1][0] = 9000;
 
@@ -47,16 +47,16 @@ vj_effect *greyselect_init(int w, int h)
     ve->limits[0][3] = 0;
     ve->limits[1][3] = 255;
 
-	ve->limits[0][4] = 0;
-	ve->limits[1][4] = 1;
+    ve->limits[0][4] = 0;
+    ve->limits[1][4] = 1;
 
-	ve->has_user = 0;
+    ve->has_user = 0;
     ve->parallel = 1;
-	ve->description = "Grayscale by Color Key (RGB)";
+    ve->description = "Grayscale by Color Key (RGB)";
     ve->extra_frame = 0;
     ve->sub_format = 1;
     ve->rgb_conv = 1;
-	ve->param_description = vje_build_param_list(ve->num_params,"Angle","Red","Green","Blue", "Swap");
+    ve->param_description = vje_build_param_list(ve->num_params,"Angle","Red","Green","Blue", "Swap");
 
     return ve;
 }
@@ -68,20 +68,20 @@ void greyselect_apply( void *ptr, VJFrame *frame, int *args ) {
     int b = args[3];
     int swap = args[4];
 
-	const int len = frame->len;
+    const int len = frame->len;
     uint8_t *fg_cb, *fg_cr;
     int accept_angle_tg;
-	int cb, cr;
+    int cb, cr;
     float kg1, tmp, aa = 255.0f, bb = 255.0f;
     float angle = (float) i_angle / 100.0f;
     unsigned int pos;
     uint8_t val;
-	uint8_t *Cb = frame->data[1];
-	uint8_t *Cr = frame->data[2];
-	int iy=0,iu=128,iv=128;
-	_rgb2yuv(r,g,b,iy,iu,iv);
-	aa = (float) iu;
-	bb = (float) iv;
+    uint8_t *Cb = frame->data[1];
+    uint8_t *Cr = frame->data[2];
+    int iy=0,iu=128,iv=128;
+    _rgb2yuv(r,g,b,iy,iu,iv);
+    aa = (float) iu;
+    bb = (float) iv;
 
     tmp = sqrt(((aa * aa) + (bb * bb)));
     cb = 255 * (aa / tmp);
@@ -95,31 +95,31 @@ void greyselect_apply( void *ptr, VJFrame *frame, int *args ) {
     fg_cb = Cb;
     fg_cr = Cr;
 
-	if( swap == 0 ) {
-		for (pos = len; pos != 0; pos--) {
-			short xx, yy;
-			xx = (((fg_cb[pos]) * cb) + ((fg_cr[pos]) * cr)) >> 7;
-			yy = (((fg_cr[pos]) * cb) - ((fg_cb[pos]) * cr)) >> 7;
-			val = (xx * accept_angle_tg) >> 4;
+    if( swap == 0 ) {
+        for (pos = len; pos != 0; pos--) {
+            short xx, yy;
+            xx = (((fg_cb[pos]) * cb) + ((fg_cr[pos]) * cr)) >> 7;
+            yy = (((fg_cr[pos]) * cb) - ((fg_cb[pos]) * cr)) >> 7;
+            val = (xx * accept_angle_tg) >> 4;
 
-			if (abs(yy) > val) {
-				Cb[pos]=128;
-				Cr[pos]=128;
-			}
-		}
-	}
-	else {
-		for (pos = len; pos != 0; pos--) {
-			short xx, yy;
-			xx = (((fg_cb[pos]) * cb) + ((fg_cr[pos]) * cr)) >> 7;
-			yy = (((fg_cr[pos]) * cb) - ((fg_cb[pos]) * cr)) >> 7;
-			val = (xx * accept_angle_tg) >> 4;
+            if (abs(yy) > val) {
+                Cb[pos]=128;
+                Cr[pos]=128;
+            }
+        }
+    }
+    else {
+        for (pos = len; pos != 0; pos--) {
+            short xx, yy;
+            xx = (((fg_cb[pos]) * cb) + ((fg_cr[pos]) * cr)) >> 7;
+            yy = (((fg_cr[pos]) * cb) - ((fg_cb[pos]) * cr)) >> 7;
+            val = (xx * accept_angle_tg) >> 4;
 
-			if (abs(yy) <= val) {
-				Cb[pos]=128;
-				Cr[pos]=128;
-			}
-		}
-	}
+            if (abs(yy) <= val) {
+                Cb[pos]=128;
+                Cr[pos]=128;
+            }
+        }
+    }
 }
 
--- veejay-current/veejay-server/libvje/effects/keyselect.c
+++ veejay-current/veejay-server/libvje/effects/keyselect.c
@@ -26,7 +26,7 @@ vj_effect *keyselect_init(int w, int h)
 {
     vj_effect *ve;
     ve = (vj_effect *) vj_calloc(sizeof(vj_effect));
-    ve->num_params = 6;
+    ve->num_params = 5;
     ve->defaults = (int *) vj_calloc(sizeof(int) * ve->num_params);	/* default values */
     ve->limits[0] = (int *) vj_calloc(sizeof(int) * ve->num_params);	/* min */
     ve->limits[1] = (int *) vj_calloc(sizeof(int) * ve->num_params);	/* max */
@@ -35,7 +35,6 @@ vj_effect *keyselect_init(int w, int h)
     ve->defaults[2] = 0;	/* g */
     ve->defaults[3] = 255;	/* b */
     ve->defaults[4] = 3;	/* blend type */
-    ve->defaults[5] = 2400;	/* noise suppression */
     ve->limits[0][0] = 1;
     ve->limits[1][0] = 9000;
 
@@ -51,15 +50,13 @@ vj_effect *keyselect_init(int w, int h)
     ve->limits[0][4] = 0;
     ve->limits[1][4] = 7;
 
-	ve->limits[0][5] = 0;
-	ve->limits[1][5] = 5500;
 	ve->has_user = 0;
     ve->parallel = 1;
 	ve->description = "Blend by Color Key (RGB)";
     ve->extra_frame = 1;
     ve->sub_format = 1;
 	ve->rgb_conv = 1;
-	ve->param_description = vje_build_param_list(ve->num_params, "Angle","Red","Green","Blue","Blend mode","Noise suppression" );
+	ve->param_description = vje_build_param_list(ve->num_params, "Angle","Red","Green","Blue","Blend mode" );
     return ve;
 }
 
@@ -67,7 +64,8 @@ typedef uint8_t(*blend_func)(uint8_t y1, uint8_t y2);
 static blend_func get_blend_func(const int mode);
 
 static uint8_t blend_func1(uint8_t a, uint8_t b) {
-	return (uint8_t)  255 - (abs(255-a-b));
+	int diff = abs( 0xff - a - b );
+	return (uint8_t)  (0xff - diff);
 }
 
 static uint8_t blend_func2(uint8_t a, uint8_t b) {
@@ -79,7 +77,7 @@ static uint8_t blend_func2(uint8_t a, uint8_t b) {
 }
 
 static uint8_t blend_func3(uint8_t a , uint8_t b) {
-	return (uint8_t) (a * b)>>8;
+	return (uint8_t) ( (uint16_t) a * b)>>8;
 }
 
 
@@ -96,24 +94,30 @@ static uint8_t blend_func5(uint8_t a, uint8_t b) {
 	uint8_t c = 0xff - b;
 	if( c == 0 )
 		return CLAMP_Y(b);
-	val = b / c;
+	val = (uint8_t)(b * 0xff / c);
 	return CLAMP_Y(val);
 }
 
 static uint8_t blend_func6(uint8_t a, uint8_t b) {
-	uint8_t val = a + (2 * (b)) - 255;
+	int val = a + (b - 0xff);
 	return CLAMP_Y(val);
 }
 
 static uint8_t blend_func7(uint8_t a, uint8_t b) {
-	return (uint8_t) ( a + ( b - 255) );
+    int val = a + (2 * b) - 255;
+    return CLAMP_Y(val);
 }
 
 static uint8_t blend_func8(uint8_t a, uint8_t b) {
-	int c;
-	if(b < 128) c = (a * b) >> 7;
-	else c = 255 - ((255-b)*(255-a)>>7);
-	return CLAMP_Y(c);
+    int c;
+
+    if (b < 128) {
+        c = (a * b) >> 7;
+    } else {
+        c = 255 - (((255 - b) * (255 - a)) >> 7);
+    }
+
+    return CLAMP_Y(c);
 }
 
 static blend_func get_blend_func(const int mode) {
@@ -140,106 +144,51 @@ void keyselect_apply( void *ptr, VJFrame *frame, VJFrame *frame2, int *args ) {
     int g = args[2];
     int b = args[3];
     int mode = args[4];
-    int i_noise = args[5];
 
-	uint8_t *fg_y, *fg_cb, *fg_cr;
-    uint8_t *bg_y, *bg_cb, *bg_cr;
 	const unsigned int width = frame->width;
 	const unsigned int height = frame->height;
-    int accept_angle_tg, accept_angle_ctg, one_over_kc;
-    int kfgy_scale, kg;
-    int cb, cr;
-    int kbg, x1, y1;
-    float kg1, tmp, aa = 255.0f, bb = 255.0f, _y = 0;
+    float tmp, aa = 255.0f, bb = 255.0f, _y = 0;
     float angle = (float) i_angle / 100.0f;
-    float noise_level = (i_noise / 100.0f);
     unsigned int pos;
-    uint8_t val, tmp1;
+    uint8_t val;
     uint8_t *Y = frame->data[0];
 	uint8_t *Cb= frame->data[1];
 	uint8_t *Cr= frame->data[2];
 	
+	uint8_t *Y2 = frame2->data[0];
+	uint8_t *U2 = frame2->data[1];
+	uint8_t *V2 = frame2->data[2];
+
 	int	iy=pixel_Y_lo_,iu=128,iv=128;
 	_rgb2yuv( r,g,b, iy,iu,iv );
 	_y = (float) iy;
 	aa = (float) iu;
 	bb = (float) iv;
-    tmp = sqrt(((aa * aa) + (bb * bb)));
-    cb =255 * (aa / tmp);
-    cr =255 * (bb / tmp);
-    kg1 = tmp;
+
+	tmp = sqrt(((aa * aa) + (bb * bb)));
+    const int cb =255 * (aa / tmp);
+    const int cr =255 * (bb / tmp);
 
     blend_func blend_pixel = get_blend_func(mode);
-
-    /* obtain coordinate system for cb / cr */
-    accept_angle_tg = (int)( 15.0f * tanf(M_PI * angle / 180.0f));
-    accept_angle_ctg= (int)( 15.0f / tanf(M_PI * angle / 180.0f));
-
-
-    tmp = 1 / kg1;
-    one_over_kc = 0xff * 2 * tmp - 0xff;
-    kfgy_scale = 0xf * (float) (_y) / kg1;
-    kg = kg1;
-
-    /* intialize pointers */
-    fg_y = Y;
-    fg_cb = Cb;
-    fg_cr = Cr;
-	/* 2005: swap these !! */
-    bg_y = frame2->data[0];
-    bg_cb = frame2->data[1];
-    bg_cr = frame2->data[2];
+    
+	const int accept_angle_tg = (int)( 15.0f * tanf(M_PI * angle / 180.0f));
 
     for (pos = (width * height); pos != 0; pos--) {
-	short xx, yy;
-	/* convert foreground to xz coordinates where x direction is
-	   defined by key color */
+		short xx, yy;
 
-	xx = (((fg_cb[pos]) * cb) + ((fg_cr[pos]) * cr)) >> 7;
-	yy = (((fg_cr[pos]) * cb) - ((fg_cb[pos]) * cr)) >> 7;
+		xx = (((Cb[pos]) * cb) + ((Cr[pos]) * cr)) >> 7;
+		yy = (((Cr[pos]) * cb) - ((Cb[pos]) * cr)) >> 7;
 
-	/* accept angle should not be > 90 degrees 
-	   reasonable results between 10 and 80 degrees.
-	 */
+		val = (xx * accept_angle_tg) >> 4;
 
-	val = (xx * accept_angle_tg) >> 4;
-	if (abs(yy) < val) {
-	    /* compute fg, suppress fg in xz according to kfg 
-	*/
-	    val = (yy * accept_angle_ctg) >> 4;
+		if (abs(yy) >= val) {
+		
+			Y[pos] = blend_pixel( Y[pos], Y2[pos ] );
 
-	    x1 = abs(val);
-	    y1 = yy;
-	    tmp1 = xx - x1;
-
-	    kbg = (tmp1 * one_over_kc) >> 1;
-
-	    val = (tmp1 * kfgy_scale) >> 4;
-	    val = fg_y[pos] - val;
-
-	    Y[pos] = val;
-
-	    // convert suppressed fg back to cbcr 
-		// cb,cr are signed, go back to unsigned !
-	    val = ((x1 * cb) - (y1 * cr)) >> 7;
-	    Cb[pos] = val;
-	    val = ((x1 * cr) - (y1 * cb)) >> 7;
-	    Cr[pos] = val;
-	    // deal with noise 
-
-	    val = (yy * yy) + (kg * kg);
-	    if (val < (noise_level * noise_level)) {
-			kbg = 255;
-	    }
-	    
-		val = (Y[pos] + (kbg * bg_y[pos])) >> 8;
-	    
-		Y[pos] = blend_pixel( val, fg_y[pos] );
-		Cb[pos] = (Cb[pos] + (kbg * bg_cb[pos])) >> 8;
-		Cr[pos] = (Cr[pos] + (kbg * bg_cr[pos])) >> 8;
-	}
+			Cb[pos] = ((Y2[pos] * (Cb[pos] - U2[pos])) >> 8) + Cb[pos];
+			Cr[pos] = ((Y2[pos] * (Cr[pos] - V2[pos])) >> 8) + Cr[pos];
+		}
     }
-
 
 }
 
