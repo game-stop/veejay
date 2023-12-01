0b726f0bc48e5868a21550a5305737c0fcb67760
veejay <>
2023-12-01 23:12:56 +0100

    reduce number of calculations to improve performance and fix parameter bounds

--- veejay-current/veejay-server/libvje/effects/widthmirror.c
+++ veejay-current/veejay-server/libvje/effects/widthmirror.c
@@ -26,19 +26,19 @@ vj_effect *widthmirror_init(int max_width,int h)
 {
     vj_effect *ve = (vj_effect *) vj_calloc(sizeof(vj_effect));
     ve->num_params = 1;
-    ve->defaults = (int *) vj_calloc(sizeof(int) * ve->num_params);	/* default values */
-    ve->limits[0] = (int *) vj_calloc(sizeof(int) * ve->num_params);	/* min */
-    ve->limits[1] = (int *) vj_calloc(sizeof(int) * ve->num_params);	/* max */
+    ve->defaults = (int *) vj_calloc(sizeof(int) * ve->num_params); /* default values */
+    ve->limits[0] = (int *) vj_calloc(sizeof(int) * ve->num_params);    /* min */
+    ve->limits[1] = (int *) vj_calloc(sizeof(int) * ve->num_params);    /* max */
     ve->defaults[0] = 2;
 
     ve->limits[0][0] = 2;
-    ve->limits[1][0] = max_width;
+    ve->limits[1][0] = max_width / 24;
 
     ve->description = "Width Mirror";
-    ve->sub_format = -1;
+    ve->sub_format = 1;
     ve->extra_frame = 0;
     ve->has_user = 0;
-	ve->param_description = vje_build_param_list( ve->num_params, "Widths");
+    ve->param_description = vje_build_param_list( ve->num_params, "Widths");
     return ve;
 }
 
@@ -48,46 +48,34 @@ void widthmirror_apply(void *ptr, VJFrame *frame, int *args)
     int width_div = args[0];
     const unsigned int width = frame->width;
     const int len = frame->len;
-    const int uv_len = frame->uv_len;
-    const int uv_width = frame->uv_width;
-    const int uv_width_div = width_div;
     int p1;
     uint8_t x1, x2, x3;
-    uint8_t *Y = frame->data[0];
-    uint8_t *Cb= frame->data[1];
-	uint8_t *Cr= frame->data[2];
+
+    uint8_t *restrict Y = frame->data[0];
+    uint8_t *restrict Cb= frame->data[1];
+    uint8_t *restrict Cr= frame->data[2];
+
     if (width_div >= frame->width || width_div < 2)
-	    width_div = 2;
+        width_div = 2;
 
-    for (r = width; r < len; r += width) {
-	unsigned int divisor = width / width_div;
-	for (c = 0; c < width; c++) {
-	    if (divisor - c < 0)
-		p1 = c - divisor + r;
-	    else
-		p1 = divisor - c + r;
-	    x1 = Y[c + r];
-	    Y[p1] = x1;
-	    Y[width - c + r] = x1;
-	}
-    }
-    for (r = uv_width; r < uv_len; r += uv_width) {
-	unsigned int divisor = uv_width / uv_width_div;
-	for (c = 0; c < uv_width; c++) {
-	    if (divisor - c < 0)
-		p1 = c - divisor + r;
-	    else
-		p1 = divisor - c + r;
-	
-	    x2 = Cb[c + r];
-	    Cb[p1] = x2;
-	    Cb[uv_width - c + r] = x2;
+    unsigned int divisor = width / width_div;
 
-	    x3 = Cr[c + r];
-	    Cr[p1] = x3;
-	    Cr[uv_width - c + r] = x3;
-	
-	}
+    for (r = 0; r < len; r += width) {
+        for (c = 0; c < width; c++) {
+            p1 = ( divisor - c < 0 ? c - divisor + r : divisor - c + r );
+
+            x1 = Y[c + r];
+            Y[p1] = x1;
+            Y[width - c + r] = x1;
+
+            x2 = Cb[c + r];
+            Cb[p1] = x2;
+            Cb[width - c + r] = x2;
+
+            x3 = Cr[c + r];
+            Cr[p1] = x3;
+            Cr[width - c + r] = x3;
+        }
     }
 }
 
