91cfebd3ec1f8bf72a4b62e02e732543d0de300a
niels <>
2026-03-04 23:09:23 +0100

    micro optimize for lightning speed

--- veejay-current/veejay-server/libvje/effects/opacitythreshold.c
+++ veejay-current/veejay-server/libvje/effects/opacitythreshold.c
@@ -18,6 +18,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307 , USA.
  */
 #include <stdint.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <veejaycore/defs.h>
 #include <libvje/vje.h>
@@ -48,61 +49,90 @@ vj_effect *opacitythreshold_init(int w, int h)
     return ve;
 }
 
-void opacitythreshold_apply( void *ptr, VJFrame *frame, VJFrame *frame2, int *args ) {
-    int opacity = args[0];
-    int threshold = args[1];
-    int t2 = args[2];
+typedef struct {
+	uint16_t *hblur;
+} op_thres_t;
 
-    const int width = frame->width;
-    unsigned int x, y;
-	const int len = frame->len-width;
-    uint8_t a1, a2;
-    unsigned int op0, op1;
-  	uint8_t *Y = frame->data[0];
-	uint8_t *Cb= frame->data[1];
-	uint8_t *Cr= frame->data[2];
-    uint8_t *Y2 = frame2->data[0];
- 	uint8_t *Cb2= frame2->data[1];
-	uint8_t *Cr2= frame2->data[2];
-
-    op1 = (opacity > 255) ? 255 : opacity;
-    op0 = 255 - op1;
-
-    for (y = width; y < len; y += width) {
-	for (x = 1; x < width-1; x++) {
-	    a1 = Y[x + y];
-	    a2 = Y2[x + y];
-	    if (a1 < threshold || a1 > t2) {
-		    a1 = (Y[y - width + x - 1] +
-			  Y[y - width + x + 1] +
-			  Y[y - width + x] +
-			  Y[y + x] +
-			  Y[y + x - 1] +
-			  Y[y + x + 1] +
-			  Y[y + width + x] +
-			  Y[y + width + x + 1] +
-			  Y[y + width + x - 1]
-			) / 9;
-
-		    a2 = (Y2[y - width + x - 1] +
-			  Y2[y - width + x + 1] +
-			  Y2[y - width + x] +
-			  Y2[y + x] +
-			  Y2[y + x - 1] +
-			  Y2[y + x + 1] +
-			  Y2[y + width + x] +
-			  Y2[y + width + x + 1] +
-			  Y2[y + width + x - 1]
-			) / 9;
-
-		    Y[x + y] = (op0 * a1 + op1 * a2) >> 8;
-		    Cb[x + y] =
-			(op0 * Cb[x + y] +
-			 op1 * Cb2[x + y]) >> 8;
-		    Cr[x + y] =
-			(op0 * Cr[x + y] +
-			 op1 * Cr2[x + y]) >> 8;
-	    }
+void *opacitythreshold_malloc(int w, int h) {
+	op_thres_t *opt = (op_thres_t*) vj_calloc(sizeof(op_thres_t));
+	if(!opt)
+		return NULL;
+	opt->hblur = (uint16_t*) vj_calloc(sizeof(uint16_t) * w * h );
+	if(!opt->hblur) {
+		free(opt);
+		return NULL;
 	}
+	return (void*) opt;
+}
+
+void opacitythreshold_free(void *ptr) {
+	op_thres_t *opt = (op_thres_t*) ptr;
+	if(opt) {
+		if(opt->hblur)
+			free(opt->hblur);
+		free(opt);
+	}
+}
+
+void opacitythreshold_apply(void *ptr, VJFrame *frame, VJFrame *frame2, int *args)
+{
+    const int opacity   = args[0];
+    const int threshold = args[1];
+    const int tmax      = args[2];
+
+    const int width  = frame->width;
+    const int height = frame->height;
+
+    const unsigned int op1 = (opacity > 255) ? 255 : opacity;
+    const unsigned int op0 = 255 - op1;
+
+    uint8_t *restrict Y   = frame->data[0];
+    uint8_t *restrict Cb  = frame->data[1];
+    uint8_t *restrict Cr  = frame->data[2];
+
+    uint8_t *restrict Y2  = frame2->data[0];
+    uint8_t *restrict Cb2 = frame2->data[1];
+    uint8_t *restrict Cr2 = frame2->data[2];
+
+    const int inv9 = 7282;
+
+	op_thres_t *opt = (op_thres_t*) ptr;
+
+    uint16_t *hblur = opt->hblur;
+
+    for (int y = 0; y < height; y++)
+    {
+        int row = y * width;
+
+        hblur[row + 0] = Y[row + 0] + Y[row + 1] + Y[row + 0];
+#pragma omp simd
+        for (int x = 1; x < width - 1; x++)
+        {
+            hblur[row + x] = Y[row + x - 1] + Y[row + x] + Y[row + x + 1];
+        }
+
+        hblur[row + width - 1] = Y[row + width - 2] + Y[row + width - 1] + Y[row + width - 1];
+    }
+
+
+    for (int y = 1; y < height - 1; y++)
+    {
+        int row = y * width;
+        int row_up    = (y - 1) * width;
+        int row_down  = (y + 1) * width;
+
+#pragma omp simd
+        for (int x = 1; x < width - 1; x++)
+        {
+            int sum = hblur[row_up + x] + hblur[row + x] + hblur[row_down + x];
+            int blur = (sum * inv9) >> 16;
+
+            if (blur < threshold || blur > tmax)
+            {
+                Y[row + x]  = (op0 * blur + op1 * Y2[row + x]) >> 8;
+                Cb[row + x] = (op0 * Cb[row + x] + op1 * Cb2[row + x]) >> 8;
+                Cr[row + x] = (op0 * Cr[row + x] + op1 * Cr2[row + x]) >> 8;
+            }
+        }
     }
 }
