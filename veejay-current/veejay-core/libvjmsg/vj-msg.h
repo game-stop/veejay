2bd02e2625daab2eaf9423439a37185cb64cd39c
niels <>
2026-03-04 22:33:45 +0100

    modern c style build fixes

--- veejay-current/veejay-core/libvjmsg/vj-msg.h
+++ veejay-current/veejay-core/libvjmsg/vj-msg.h
@@ -28,22 +28,24 @@ enum {
     VEEJAY_MSG_DEBUG = 4,
 };
 
-extern void veejay_print_backtrace();
+extern void veejay_print_backtrace(void);
+extern void veejay_print_banner(void);
 extern void veejay_strrep(char *s, char delim, char tok);
 extern void veejay_msg(int type, const char format[], ...);
-extern int veejay_is_colored();
+extern int veejay_is_colored(void);
 extern void veejay_set_debug_level(int level);
-extern int veejay_get_debug_level();
+extern int veejay_get_debug_level(void);
 extern void veejay_set_colors(int level);
-extern void veejay_silent();
-extern int veejay_is_silent();
+extern void veejay_silent(void);
+extern int veejay_is_silent(void);
 extern int veejay_get_file_ext( char *file, char *dst, int dlen);
 extern void veejay_chomp_str( char *str, int *dlen );
 extern int has_env_setting( const char *env, const char *value );
-extern char* veejay_msg_ringfetch();
-extern int veejay_log_to_ringbuffer();
-extern void veejay_toggle_osl();
-extern void veejay_init_msg_ring();
-extern void veejay_destroy_msg_ring();
+extern char* veejay_msg_ringfetch(void);
+extern int veejay_log_to_ringbuffer(void);
+extern void veejay_toggle_osl(void);
+extern void veejay_init_msg_ring(void);
+extern void veejay_destroy_msg_ring(void);
+extern void veejay_set_timestamp(int status);
 #endif
 
