33bffd2c5dac699b72da9e433656bca4ca760e95
niels <>
2026-03-04 23:32:09 +0100

    micro optimize and have libvje advise on num_threads for parallization with openmp

--- veejay-current/veejay-server/libvje/effects/edgeglow.c
+++ veejay-current/veejay-server/libvje/effects/edgeglow.c
@@ -67,17 +67,23 @@ typedef struct
 {
     uint8_t *buf;
 	uint8_t *blurmask;
+    int n_threads;
 } edgeglow_t;
 
 void *edgeglow_malloc(int w, int h) {
     edgeglow_t *s = (edgeglow_t*) vj_malloc(sizeof(edgeglow_t));
     if(!s) return NULL;
+
     s->buf = (uint8_t*) vj_malloc(sizeof(uint8_t) * w * h * 2 );
     if(!s->buf) {
         free(s);
         return NULL;
     }
-	s->blurmask = s->buf + (w*h);
+
+    s->blurmask = s->buf + (w*h);
+
+    const int len = w * h;
+    s->n_threads = vje_advise_num_threads(len);
 
     return (void*) s;
 }
@@ -88,101 +94,109 @@ void edgeglow_free(void *ptr) {
     free(s);
 }
 
-
-void edgeglow_apply( void *ptr, VJFrame *frame, int *args ) {
+void edgeglow_apply(void *ptr, VJFrame *frame, int *args)
+{
     edgeglow_t *s = (edgeglow_t*) ptr;
-    const int t = args[0];
-    const int threshold = (args[0] * args[0]);
-	const int red = args[1];
-	const int green = args[2];
-	const int blue = args[3];
-	const float scalingFactor = (args[4] * 0.1f);
 
-    const int len = frame->len;
-    const int width = frame->width;
+    const int t = args[0];
+    const int red   = args[1];
+    const int green = args[2];
+    const int blue  = args[3];
+    const float scalingFactor = (args[4] * 0.1f);
+
+    const int len    = frame->len;
+    const int width  = frame->width;
     const int height = frame->height;
 
-    uint8_t *restrict Y = frame->data[0];
+    uint8_t *restrict Y  = frame->data[0];
     uint8_t *restrict Cb = frame->data[1];
     uint8_t *restrict Cr = frame->data[2];
-    uint8_t *restrict B = s->buf;
-	uint8_t *restrict C = s->blurmask;
+    uint8_t *restrict B  = s->buf;
+    uint8_t *restrict C  = s->blurmask;
 
-	int nY=0,nU=128,nV=128;
-	
-	_rgb2yuv( red,green,blue, nY, nU, nV );
+    int nY = 0, nU = 128, nV = 128;
+    _rgb2yuv(red, green, blue, nY, nU, nV);
 
-	int L2 = (nY * 100) >> 8;
-	int a2 = (((nU - 128) * 127) >> 8);
-	int b2 = (((nV - 128) * 127) >> 8);
+    const int L2 = (nY * 100) >> 8;
+    const int a2 = ((nU - 128) * 127) >> 8;
+    const int b2 = ((nV - 128) * 127) >> 8;
 
+    veejay_memset(B, 0, width);
+    veejay_memset(B + (height - 1) * width, 0, width);
 
-	for( int y = 0; y < 1; y ++ ) {
-		for( int x = 0; x < width; x ++ )
-			B[y*width+x] = 0;
-	}
-
-	for( int y = (height-1); y < height; y ++ ) {
-		for( int x = 0; x < width; x ++ ) {
-			B[y*width+x] = 0;
-		}
-	}
-
-	// edge detect
+#pragma omp parallel for num_threads(s->n_threads) schedule(static)
     for (int y = 1; y < height - 1; ++y) {
-		B[ y * width ] = 0;
+
+        const int row = y * width;
+        B[row] = 0;
+
 #pragma omp simd
         for (int x = 1; x < width - 1; ++x) {
-            const int index = y * width + x;
 
-            const int gx = Y[index - width - 1] - Y[index - width + 1] + 2 * (Y[index - 1] - Y[index + 1]) + Y[index + width - 1] - Y[index + width + 1];
-            const int gy = Y[index - width - 1] + 2 * Y[index - width] + Y[index - width + 1] - Y[index + width - 1] - 2 * Y[index + width] - Y[index + width + 1];
+            const int idx = row + x;
+
+            const int gx =
+                Y[idx - width - 1] - Y[idx - width + 1] +
+                2 * (Y[idx - 1] - Y[idx + 1]) +
+                Y[idx + width - 1] - Y[idx + width + 1];
+
+            const int gy =
+                Y[idx - width - 1] + 2 * Y[idx - width] + Y[idx - width + 1] -
+                Y[idx + width - 1] - 2 * Y[idx + width] - Y[idx + width + 1];
 
             const int abs_gx = (gx ^ (gx >> 31)) - (gx >> 31);
             const int abs_gy = (gy ^ (gy >> 31)) - (gy >> 31);
 
-            const int gradientMagnitude = abs_gx + abs_gy;
-            const int normMagnitude = (gradientMagnitude * 255) / 1020;
+            const int grad = abs_gx + abs_gy;
+            const int norm = (grad * 255) / 1020;
 
-            B[index] = (normMagnitude > t) ? gradientMagnitude: 0;
+            const int mask = -(norm > t);
+
+            B[idx] = grad & mask;
         }
-		B[ y * width + width ] = 0;
+
+        B[row + width - 1] = 0;
     }
 
-	// blur edge mask
-	for (int y = 1; y < height - 1; ++y) {
-    	#pragma omp simd
-    	for (int x = 1; x < width - 1; ++x) {
-            const int index = y*width + x;
-            const int sum = B[index - width - 1] + B[index - width] + B[index - width + 1]
-                          + B[index - 1] + B[index] + B[index + 1]
-                          + B[index + width - 1] + B[index + width] + B[index + width + 1];
-            C[index] = sum / 9;
-    	}
-	}
+#pragma omp parallel for num_threads(s->n_threads) schedule(static)
+    for (int y = 1; y < height - 1; ++y) {
 
+        const int row = y * width;
 
-    #pragma omp simd
-    for( int i = 0; i < len; i ++ ) {
-        
+#pragma omp simd
+        for (int x = 1; x < width - 1; ++x) {
+
+            const int idx = row + x;
+
+            const int sum =
+                B[idx - width - 1] + B[idx - width] + B[idx - width + 1] +
+                B[idx - 1]         + B[idx]         + B[idx + 1] +
+                B[idx + width - 1] + B[idx + width] + B[idx + width + 1];
+
+            C[idx] = sum / 9;
+        }
+    }
+
+#pragma omp parallel for num_threads(s->n_threads) schedule(static)
+    for (int i = 0; i < len; ++i) {
+
         const int edgeIntensity = (int)(C[i] * scalingFactor);
+        const int mask = -(edgeIntensity > 0);
 
         int L1 = (Y[i] * 100) >> 8;
-        int a1 = (((Cb[i] - 128) * 127) >> 8);
-        int b1 = (((Cr[i] - 128) * 127) >> 8);
+        int a1 = ((Cb[i] - 128) * 127) >> 8;
+        int b1 = ((Cr[i] - 128) * 127) >> 8;
 
         L1 += ((L2 - L1) * edgeIntensity) / 255;
         a1 += ((a2 - a1) * edgeIntensity) / 255;
         b1 += ((b2 - b1) * edgeIntensity) / 255;
 
-        const int L_out  = CLAMP_Y((L1*255)/100);
-        const int a_out  = CLAMP_UV(a1 + 128);
-        const int b_out  = CLAMP_UV(b1 + 128);
+        const int L_out = (L1 * 255) / 100;
+        const int a_out = a1 + 128;
+        const int b_out = b1 + 128;
 
-        Y[i]  = (uint8_t)(edgeIntensity > 0 ? L_out : Y[i]);
-        Cb[i] = (uint8_t)(edgeIntensity > 0 ? a_out : Cb[i]);
-        Cr[i] = (uint8_t)(edgeIntensity > 0 ? b_out : Cr[i]);
-		
+        Y[i]  = (uint8_t)((L_out & mask) | (Y[i]  & ~mask));
+        Cb[i] = (uint8_t)((a_out & mask) | (Cb[i] & ~mask));
+        Cr[i] = (uint8_t)((b_out & mask) | (Cr[i] & ~mask));
     }
-
 }
