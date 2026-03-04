a193cf40c499f3150a7c5399bcc144f9b5f5a4f9
niels <>
2026-03-05 00:25:36 +0100

    expose bio2jack functions

--- veejay-current/veejay-server/veejay/vj-jack.h
+++ veejay-current/veejay-server/veejay/vj-jack.h
@@ -27,37 +27,76 @@ int vj_jack_init(editlist *el);
 
 int vj_jack_update_buffer( uint8_t *buff, int bps, int num_channels, int buf_len);
 
-int vj_jack_stop();
+int vj_jack_stop(void);
 
-int vj_jack_start();
+int vj_jack_start(void);
 
-int vj_jack_pause();
+int vj_jack_pause(void);
 
-int vj_jack_resume();
+int vj_jack_resume(void);
 
 int	vj_jack_play(void *data, int len);
 
 int	vj_jack_set_volume(int volume);
 
-int	vj_jack_pause();
+int	vj_jack_pause(void);
 
-int	vj_jack_resume();
+int	vj_jack_resume(void);
 
 long	vj_jack_get_status(long int *sec, long int *usec);
 
-void	vj_jack_enable();
+void	vj_jack_enable(void);
 
-void	vj_jack_disable();
+void	vj_jack_disable(void);
 
-int	vj_jack_get_space();
+int	vj_jack_get_space(void);
 
-int vj_jack_continue(int speed);
+int vj_jack_initialize(void);
 
-int vj_jack_initialize();
+int  vj_jack_get_client_samplerate(void);
 
-int  vj_jack_rate();
+int  vj_jack_get_rate(void);
 
-void	vj_jack_reset();
+void	vj_jack_reset(void);
+
+void vj_jack_debug(int skipv, int skipa);
+
+double vj_jack_get_played_position(void);
+
+int     vj_jack_get_ringbuffer_frames_free(void);
+
+int     vj_jack_get_ringbuffer_size(void);
+
+int     vj_jack_get_period_size(void);
+
+int vj_jack_client_to_jack_frames(int client_frames);
+
+long    vj_jack_get_written_frames(void);
+
+long vj_jack_get_required_free_frames(int client_frames);
+
+long vj_jack_get_ringbuffer_used(void);
+
+unsigned long vj_jack_get_played_frames(void);
+
+int vj_jack_is_stopped(void);
+int vj_jack_is_playing(void);
+
+unsigned long vj_jack_get_bytes_per_frame(void);
+
+int vj_jack_is_callback_active(void);
+
+void vj_jack_flush(void);
+
+double vj_jack_get_total_latency(void);
+
+long vj_jack_underruns(void);
+
+long vj_jack_get_bytes_stored_from_driver(void);
+
+int vj_jack_is_running(void);
+
+int vj_jack_xrun_flag(void);
 
 #endif
 #endif
--- veejay-current/veejay-server/veejay/vj-pjack.c
+++ veejay-current/veejay-server/veejay/vj-pjack.c
@@ -20,31 +20,30 @@
 #include <config.h>
 #ifdef HAVE_JACK
 #include <stdint.h>
+#include <sys/time.h>
 #include <veejaycore/defs.h>
 #include <bio2jack/bio2jack.h>
 #include <libel/vj-el.h>
 static int driver = 0;
-static int bits_per_sample = 0;
-static unsigned long audio_rate = 0;
-static int audio_channels = 0;
+
 extern void veejay_msg(int type, const char format[], ...);
 
-int vj_jack_initialize()
+int vj_jack_initialize(void)
 {
 	JACK_Init();
 	JACK_SetClientName("veejay");
 	return 0;
 }
-static int _vj_jack_start(int *dri)
+static int _vj_jack_start(int *dri, int bits_per_sample, long audio_rate, int audio_channels, double SPVF)
 {
-	int err = JACK_Open(dri, bits_per_sample,&audio_rate,audio_channels);
+	int err = JACK_Open(dri, (bits_per_sample * 8) / audio_channels,&audio_rate,audio_channels, SPVF);
 	if(err == ERR_RATE_MISMATCH)
 	{
 		veejay_msg(1, "(Jackd) Sample rate mismatch (Retrying)");
 
 		JACK_Close( *dri );
 
-		err = JACK_Open(dri, bits_per_sample,&audio_rate, audio_channels);
+		err = JACK_Open(dri, (bits_per_sample * 8) / audio_channels,&audio_rate, audio_channels, SPVF);
 	}
 
 	if(err != ERR_SUCCESS)
@@ -76,10 +75,7 @@ static int _vj_jack_start(int *dri)
 
 int vj_jack_init(editlist *el)
 {
-	bits_per_sample = 16;
-	audio_channels = el->audio_chans;
-
-	if( !_vj_jack_start(&driver) )
+	if( !_vj_jack_start(&driver, el->audio_bps, el->audio_rate, el->audio_chans,1.0 / (double) el->video_fps) )
 		return 0;
 
 	long jack_rate = JACK_GetSampleRate(driver );
@@ -88,39 +84,19 @@ int vj_jack_init(editlist *el)
 		veejay_msg(2,"May be wrong but bio2jack client reports a different sample rate: %ld instead of %d", jack_rate,el->audio_rate );
 	}
 
-	JACK_SetState(driver, PAUSED );
-
 	return 1;
 }
 
-int	vj_jack_rate()
+int	vj_jack_get_client_samplerate(void)
 {
 	return JACK_GetSampleRate(driver);
 }
 
-
-int	vj_jack_continue(int speed)
+int	vj_jack_stop(void)
 {
-	if(speed==0)
-	{
-		if(JACK_GetState(driver) == PAUSED) 
-			return 1;
+	if( JACK_GetState(driver) == PLAYING )
+		JACK_SetState(driver,STOPPED);
 
-		JACK_SetState(driver, PAUSED );
-		return 1;
-	}
-
-	if( JACK_GetState(driver) == PAUSED )
-	{
-		JACK_SetState(driver, PLAYING);
-		return 1;
-	}
-	return 0;
-}
-
-
-int	vj_jack_stop()
-{
 	JACK_Reset( driver );
 
 	if(JACK_Close(driver))
@@ -131,13 +107,20 @@ int	vj_jack_stop()
 	return 1;
 }
 
-void	vj_jack_enable()
-{
-	if( JACK_GetState(driver) == STOPPED )
-		JACK_SetState(driver, PLAYING );
+long vj_jack_underruns(void) {
+	return JACK_GetUnderruns(driver);
 }
 
-void	vj_jack_disable()
+int vj_jack_xrun_flag(void) {
+	return JACK_XRUNHandled(driver);
+}
+
+void	vj_jack_enable(void)
+{
+	JACK_SetState(driver, PLAYING );
+}
+
+void	vj_jack_disable(void)
 {
 	JACK_SetState( driver, STOPPED );
 }
@@ -147,13 +130,33 @@ int	vj_jack_c_play(void *data, int len, int entry)
 	return 0;
 }
 
-int	vj_jack_play(void *data, int len)
+int vj_jack_play(void *data, int len)
+{
+    int frames = JACK_Write( driver, data, len );
+    
+    // Original error-handling logic
+    if( frames == 0 && JACK_GetState(driver) == PLAYING ) {
+        vj_jack_disable();
+		veejay_msg(0, "Error writing to JACK!");
+    }
+    return frames;
+}
+
+int vj_jack_get_rate(void) {
+	return JACK_GetSampleRateJack(driver);
+}
+
+int	vj_jack_play2(void *data, int len)
 {
 	int res = JACK_Write( driver, data, len );
 	if( res == 0 && JACK_GetState(driver) == PLAYING ) {
 		vj_jack_disable();
 	}
 	return res;
+}
+
+void vj_jack_debug(int skipv, int skipa) {
+	
 }
 
 int	vj_jack_set_volume(int volume)
@@ -163,19 +166,23 @@ int	vj_jack_set_volume(int volume)
 	return 0;
 }
 
-int	vj_jack_pause()
+void vj_jack_flush(void) {
+	JACK_Flush(driver);
+}
+
+int	vj_jack_pause(void)
 {
 	JACK_SetState(driver,PAUSED);
 	return 1;
 }
 
-int	vj_jack_resume()
+int	vj_jack_resume(void)
 {
 	JACK_SetState(driver,PLAYING);
 	return 1;
 }
 
-int	vj_jack_get_space()
+int	vj_jack_get_space(void)
 {
 	return JACK_GetBytesFreeSpace(driver);
 }
@@ -185,8 +192,79 @@ long	vj_jack_get_status(long int *sec, long int *nsec)
 	return JACK_OutputStatus( driver, sec, nsec);
 }
 
-void	vj_jack_reset()
+double	vj_jack_get_played_position(void) {
+
+	return JACK_GetPlayedPosition(driver);
+}
+
+unsigned long vj_jack_get_played_frames(void) {
+	return JACK_GetPlayedFramesFromDriver(driver);
+}
+
+long    vj_jack_get_written_frames(void) {
+	return JACK_GetPosition(driver,0,FRAMES_WRITTEN_TO_JACK);
+}
+
+void	vj_jack_reset(void)
 {
 	JACK_ResetBuffer( driver );
 }
+
+int	vj_jack_is_starving(void) {
+	return JACK_BufferIsStarving(driver);
+}
+
+int	vj_jack_get_period_size(void) {
+	return JACK_GetPeriodSize(driver);
+}
+
+int	vj_jack_get_ringbuffer_frames_free(void) {
+	return JACK_GetRingBufferFreeFrames(driver);
+}
+
+int vj_jack_get_ringbuffer_size(void) {
+	return JACK_GetRingBufferSize(driver);
+}
+
+long vj_jack_get_ringbuffer_used(void) {
+	return JACK_GetRingBufferUsed(driver);
+}
+
+int vj_jack_client_to_jack_frames(int client_frames) {
+	return JACK_GetClientToJackFrames(driver,client_frames);
+}
+
+long vj_jack_get_required_free_frames(int client_frames) {
+	return JACK_GetRequiredFreeFrames(driver,client_frames);
+}
+
+unsigned long vj_jack_get_bytes_per_frame(void) {
+	return JACK_GetBytesPerOutputFrame(driver);
+}
+
+long vj_jack_get_bytes_stored_from_driver(void) {
+	return JACK_GetBytesStored(driver);
+
+}
+
+int vj_jack_is_stopped(void) {
+	return JACK_GetState(driver) == STOPPED;
+}
+int vj_jack_is_playing(void) {
+	return JACK_GetState(driver) == PLAYING;
+}
+
+int vj_jack_is_callback_active(void) {
+	return JACK_GetCallbackActive(driver);
+}
+
+double vj_jack_get_total_latency(void) {
+	return JACK_GetTotalLatency(driver);
+}
+
+
+int vj_jack_is_running(void) {
+    return (driver == 0 ? 0 : 1);
+}
+
 #endif
