51cb5ee8a745f7fd025993d1aa5d9e995748dc3d
niels <>
2026-03-04 23:16:27 +0100

    micro optimize for lightning speed

--- veejay-current/veejay-server/libvje/effects/3bar.c
+++ veejay-current/veejay-server/libvje/effects/3bar.c
@@ -45,22 +45,23 @@ vj_effect *bar_init(int width, int height)
     ve->limits[1][2] = height;
 
     ve->limits[0][3] = 0;
-    ve->limits[1][3] = height;
+    ve->limits[1][3] = width;
     ve->limits[0][4] = 0;
-    ve->limits[1][4] = height;
+    ve->limits[1][4] = width;
     ve->description = "Horizontal Sliding Bars";
 
     ve->extra_frame = 1;
 	ve->has_user = 0;
+    ve->parallel = 0;
 	ve->param_description = vje_build_param_list(ve->num_params, "Divider", "Top Y", "Bot Y", "Top X", "Bot X");
     return ve;
 }
 
 typedef struct {
-    int bar_top_auto;
-    int bar_bot_auto;
-    int bar_top_vert;
-    int bar_bot_vert;
+    int bar_top_auto_x;
+    int bar_bot_auto_x;
+    int bar_top_auto_y;
+    int bar_bot_auto_y;
 } bar_t;
 
 void *bar_malloc(int w, int h) {
@@ -71,85 +72,81 @@ void bar_free(void *ptr) {
     free(ptr);
 }
 
-void bar_apply(void *ptr, VJFrame *frame, VJFrame *frame2, int *args) {
-    int divider = args[0];
-    int top_y   = args[1];
-    int bot_y   = args[2];
-    int top_x   = args[3];
-    int bot_x   = args[4];
+void bar_apply(void *ptr, VJFrame *frame, VJFrame *frame2, int *args)
+{
+    const int divider   = args[0];
+    const int top_y_arg = args[1];
+    const int bot_y_arg = args[2];
+    const int top_x_arg = args[3];
+    const int bot_x_arg = args[4];
 
-    bar_t *bar = (bar_t *)ptr;
+    bar_t *bar = (bar_t *) ptr;
 
     const unsigned int width  = frame->width;
     const unsigned int height = frame->height;
 
-    const unsigned int top_height    = height / divider;
+    const unsigned int safe_divider = (divider > 0) ? divider : 1;
+    const unsigned int top_height    = height / safe_divider;
     const unsigned int bottom_height = height - top_height;
-    const unsigned int top_width     = width;
-    const unsigned int bottom_width  = width;
 
-    uint8_t *Y  = frame->data[0];
-    uint8_t *Cb = frame->data[1];
-    uint8_t *Cr = frame->data[2];
+    uint8_t *restrict Y  = frame->data[0];
+    uint8_t *restrict Cb = frame->data[1];
+    uint8_t *restrict Cr = frame->data[2];
 
-    uint8_t *Y2  = frame2->data[0];
-    uint8_t *Cb2 = frame2->data[1];
-    uint8_t *Cr2 = frame2->data[2];
+    const uint8_t *restrict Y2  = frame2->data[0];
+    const uint8_t *restrict Cb2 = frame2->data[1];
+    const uint8_t *restrict Cr2 = frame2->data[2];
 
-    int dst_y_offset = bar->bar_top_auto + top_y;
-    int dst_x_offset = bar->bar_top_vert + top_x;
+    bar->bar_top_auto_y = (bar->bar_top_auto_y + top_y_arg) % top_height;
+    bar->bar_top_auto_x = (bar->bar_top_auto_x + top_x_arg) % width;
 
-    if (dst_y_offset >= width) {
-        dst_y_offset = 0;
-        bar->bar_top_auto = 0;
-    }
-    if (dst_x_offset >= top_height) {
-        dst_x_offset = 0;
-        bar->bar_top_vert = 0;
-    }
+    for (unsigned int y = 0; y < top_height; y++)
+    {
+        unsigned int src_y = (y + bar->bar_top_auto_y) % top_height;
+        
+        uint8_t *dY  = Y  + y * width;
+        uint8_t *dCb = Cb + y * width;
+        uint8_t *dCr = Cr + y * width;
 
-    for (unsigned int y = 0; y < top_height; y++) {
-        unsigned int src_y = y + dst_x_offset;
-        for (unsigned int x = 0; x < top_width; x++) {
-            unsigned int src_x = x + dst_y_offset;
-            unsigned int dst_idx = y * width + x;
-            unsigned int src_idx = src_y * width + src_x;
+        const uint8_t *sY  = Y2  + src_y * width;
+        const uint8_t *sCb = Cb2 + src_y * width;
+        const uint8_t *sCr = Cr2 + src_y * width;
 
-            Y[dst_idx]  = Y2[src_idx];
-            Cb[dst_idx] = Cb2[src_idx];
-            Cr[dst_idx] = Cr2[src_idx];
+        for (unsigned int x = 0; x < width; x++)
+        {
+            unsigned int src_x = (x + bar->bar_top_auto_x) % width;
+            dY[x]  = sY[src_x];
+            dCb[x] = sCb[src_x];
+            dCr[x] = sCr[src_x];
         }
     }
 
-    dst_y_offset = bar->bar_bot_auto + bot_y;
-    dst_x_offset = bar->bar_bot_vert + bot_x;
+    if (bottom_height > 0) {
+        bar->bar_bot_auto_y = (bar->bar_bot_auto_y + bot_y_arg) % bottom_height;
+        bar->bar_bot_auto_x = (bar->bar_bot_auto_x + bot_x_arg) % width;
 
-    if (dst_y_offset >= width) {
-        dst_y_offset = 0;
-        bar->bar_bot_auto = 0;
-    }
-    if (dst_x_offset >= bottom_height) {
-        dst_x_offset = 0;
-        bar->bar_bot_vert = 0;
-    }
+        const unsigned int bottom_start = top_height * width;
 
-    for (unsigned int y = 0; y < bottom_height; y++) {
-        unsigned int src_y = y + dst_x_offset;
-        for (unsigned int x = 0; x < bottom_width; x++) {
-            unsigned int src_x = x + dst_y_offset;
-            unsigned int dst_idx = (y + top_height) * width + x;
-            unsigned int src_idx = src_y * width + src_x;
+        for (unsigned int y = 0; y < bottom_height; y++)
+        {
+            unsigned int src_y = (y + bar->bar_bot_auto_y) % bottom_height;
+            
+            uint8_t *dY  = Y  + bottom_start + y * width;
+            uint8_t *dCb = Cb + bottom_start + y * width;
+            uint8_t *dCr = Cr + bottom_start + y * width;
 
-            Y[dst_idx]  = Y2[src_idx];
-            Cb[dst_idx] = Cb2[src_idx];
-            Cr[dst_idx] = Cr2[src_idx];
+            const uint8_t *sY  = Y2  + bottom_start + src_y * width;
+            const uint8_t *sCb = Cb2 + bottom_start + src_y * width;
+            const uint8_t *sCr = Cr2 + bottom_start + src_y * width;
+
+            for (unsigned int x = 0; x < width; x++)
+            {
+                unsigned int src_x = (x + bar->bar_bot_auto_x) % width;
+                dY[x]  = sY[src_x];
+                dCb[x] = sCb[src_x];
+                dCr[x] = sCr[src_x];
+            }
         }
     }
-
-    /* update bars */
-    bar->bar_top_auto  += top_y;
-    bar->bar_bot_auto  += bot_y;
-    bar->bar_top_vert  += top_x;
-    bar->bar_bot_vert  += bot_x;
 }
 
