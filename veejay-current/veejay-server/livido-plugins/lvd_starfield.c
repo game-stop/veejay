80f0914a263afc8325925bbee89663557cccbc31
niels <>
2026-03-05 00:09:35 +0100

    micro optimize, allow user to select rand mode

--- veejay-current/veejay-server/livido-plugins/lvd_starfield.c
+++ veejay-current/veejay-server/livido-plugins/lvd_starfield.c
@@ -39,78 +39,85 @@ typedef struct
   float xpos, ypos;
   short zpos, speed;
   uint8_t color;
+  uint32_t seed;
 } STAR;
 
 typedef struct
 {
-	STAR **stars;
+	STAR *stars;
 } starfield_t;
 
-static void init_star(STAR* star, int i, int speed)
+static inline uint32_t xorshift32(uint32_t *state) {
+    uint32_t x = *state;
+    x ^= x << 13;
+    x ^= x >> 17;
+    x ^= x << 5;
+    *state = x;
+    return x;
+}
+
+static inline uint32_t fastrand_lcg(uint32_t *state) {
+    *state = (*state * 1103515245 + 12345) & 0x7fffffff;
+    return *state;
+}
+
+static inline float get_rand_float(uint32_t *seed, int mode) {
+    if (mode == 0)
+        return (float)rand() / (float)RAND_MAX;
+    else if (mode == 1)
+        return (float)(xorshift32(seed) & 0xFFFFFF) / 16777216.0f;
+    else
+        return (float)fastrand_lcg(seed) / 2147483648.0f;
+}
+
+static void init_star(STAR* star, int speed, int initial_z, int mode)
 {
-  /* randomly init stars, generate them around the center of the screen */
-  
-  star->xpos =  -10.0 + (20.0 * (rand()/(RAND_MAX+1.0)));
-  star->ypos =  -10.0 + (20.0 * (rand()/(RAND_MAX+1.0)));
-  
-  star->xpos *= 3072.0; /*change viewpoint */
-  star->ypos *= 3072.0;
+    star->zpos = (initial_z == -1) ? MAX_STARS : initial_z;
 
-  star->zpos =  i;
-  star->speed = (speed+2) + (int)( ( (double)speed) * (rand()/(RAND_MAX+1.0)));
-
-  star->color = i >> 2; /*the closer to the viewer the brighter*/
+    star->xpos = (-10.0f + (20.0f * get_rand_float(&star->seed, mode))) * 3072.0f;
+    star->ypos = (-10.0f + (20.0f * get_rand_float(&star->seed, mode))) * 3072.0f;
+    
+    star->speed = (speed + 2) + (int)((float)speed * get_rand_float(&star->seed, mode));
+    
+    star->color = (uint8_t)(255 - (star->zpos >> 3)); 
 }
 
 
-
-int	init_instance( livido_port_t *my_instance )
+int init_instance( livido_port_t *my_instance )
 {
-	starfield_t *starfield = (starfield_t*) livido_malloc( sizeof(starfield_t));
-    if(!starfield) {
-        return LIVIDO_ERROR_MEMORY_ALLOCATION;
-    }
-    starfield->stars = (STAR**) livido_malloc(sizeof(STAR*) * MAX_STARS );
+    starfield_t *starfield = (starfield_t*) livido_malloc( sizeof(starfield_t));
+    if(!starfield) return LIVIDO_ERROR_MEMORY_ALLOCATION;
+
+    starfield->stars = (STAR*) livido_malloc(sizeof(STAR) * MAX_STARS);
     if(!starfield->stars) {
-        free(starfield);
+        livido_free(starfield);
         return LIVIDO_ERROR_MEMORY_ALLOCATION;
     }
 
-	int i,j;
-	for( i = 0; i < MAX_STARS ; i ++ ) {
-		starfield->stars[i] = (STAR*) livido_malloc( sizeof(STAR) );
-        if(starfield->stars[i] == NULL) {
-            for(j = 0; j < i; j ++ ) {
-                free(starfield->stars[j]);
-            }
-            free(starfield->stars);
-            free(starfield);
-            return LIVIDO_ERROR_MEMORY_ALLOCATION;
-        }
-		init_star( starfield->stars[i], i + 1 , 2 );
-	}
+    srand((unsigned int)time(NULL));
+    uint32_t global_seed = (uint32_t)rand();
 
-	livido_property_set( my_instance, "PLUGIN_private", LIVIDO_ATOM_TYPE_VOIDPTR,1, &starfield);
+    for(int i = 0; i < MAX_STARS ; i ++ ) {
+        STAR *s = &starfield->stars[i];
+        s->seed = global_seed + i;
+        int random_depth = (rand() % MAX_STARS) + 1;
+        init_star(s, 2, random_depth, 0);
+    }
 
-	return LIVIDO_NO_ERROR;
+    livido_property_set(my_instance, "PLUGIN_private", LIVIDO_ATOM_TYPE_VOIDPTR, 1, &starfield);
+    return LIVIDO_NO_ERROR;
 }
 
-
-livido_deinit_f	deinit_instance( livido_port_t *my_instance )
+livido_deinit_f deinit_instance( livido_port_t *my_instance )
 {
-	void *ptr = NULL;
+    void *ptr = NULL;
     if ( livido_property_get( my_instance, "PLUGIN_private", 0, &ptr ) == LIVIDO_NO_ERROR )
     {
-		starfield_t *starfield = (starfield_t*) ptr;
-		int i;
-		for( i = 0; i < MAX_STARS; i ++ ) {
-			livido_free(starfield->stars[i]);
-		}
-		livido_free(starfield->stars);
-		livido_free(starfield);
-	}
-
-	return LIVIDO_NO_ERROR;
+        starfield_t *starfield = (starfield_t*) ptr;
+        livido_free(starfield->stars); // Single free for the whole block
+        livido_free(starfield);
+    }
+    return LIVIDO_NO_ERROR;
 }
 
 
@@ -136,8 +143,10 @@ int		process_instance( livido_port_t *my_instance, double timecode )
     	return LIVIDO_ERROR_INTERNAL;
 
 	//@ get parameter values
-	int		number_of_stars =  lvd_extract_param_index( my_instance,"in_parameters", 0 );
-	int		speed = 2 + lvd_extract_param_index( my_instance, "in_parameters", 1 );
+	int	num_stars =  lvd_extract_param_index( my_instance,"in_parameters", 0 );
+	int	speed = 2 + lvd_extract_param_index( my_instance, "in_parameters", 1 );
+	int mode = lvd_extract_param_index( my_instance, "in_parameters", 2 );
+	const int n = (num_stars > MAX_STARS) ? MAX_STARS : num_stars;
 
 	livido_memset( O[0], 0, len );
 	livido_memset( O[1], 128, uv_len );
@@ -146,30 +155,28 @@ int		process_instance( livido_port_t *my_instance, double timecode )
 	int center_x = w >> 1;
 	int center_y = h >> 1;
 	int i;
-	int temp_x, temp_y;
+
+	STAR *s = starfield->stars;
+
+	for(int i = 0; i < n; i++) {
+        s[i].zpos -= s[i].speed;
+
+        if( s[i].zpos <= 0 ) {
+            init_star(&s[i], speed, -1, mode);
+        }
+
+        float inv_z = 1.0f / (float)s[i].zpos;
+        int tx = (int)(s[i].xpos * inv_z) + center_x;
+        int ty = (int)(s[i].ypos * inv_z) + center_y;
+
+        if( (unsigned int)tx < (unsigned int)w && (unsigned int)ty < (unsigned int)h ) {
+            s[i].color = (uint8_t)(255 - (s[i].zpos >> 3));
+            O[0][ty * w + tx] = s[i].color;
+        } else {
+            init_star(&s[i], speed, -1, mode);
+        }
+    }
 	
-	STAR **stars = starfield->stars;
-
-	for( i = 0; i < number_of_stars; i ++ ) {
-			
-		stars[i]->zpos -= stars[i]->speed;
-
-		if( stars[i]->zpos <= 0 ) {
-			init_star( stars[i], i + 1, speed );
-		}
-
-		temp_x = (stars[i]->xpos / stars[i]->zpos ) + center_x;
-		temp_y = (stars[i]->ypos / stars[i]->zpos ) + center_y;
-
-		if( temp_x < 0 || temp_x > (w-1) || temp_y < 0 || temp_y > (h-1) ) {
-			init_star( stars[i], i + 1, speed );
-			continue;
-		}
-
-		O[0][ temp_y * w + temp_x ] = stars[i]->color;
-	}
-	
-
 	return LIVIDO_NO_ERROR;
 }
 
@@ -245,8 +252,17 @@ livido_port_t	*livido_setup(livido_setup_t list[], int version)
 		livido_set_int_value( port, "max", 64 );
 		livido_set_int_value( port, "default", 0 );
 		livido_set_string_value( port, "description" ,"Speed");
+
+
+	in_params[2] = livido_port_new( LIVIDO_PORT_TYPE_PARAMETER_TEMPLATE );
+    livido_set_string_value(in_params[2], "name", "Random Mode" );
+    livido_set_string_value(in_params[2], "kind", "INDEX" );
+    livido_set_int_value( in_params[2], "min", 0 );
+    livido_set_int_value( in_params[2], "max", 2 );
+    livido_set_int_value( in_params[2], "default", 2 );
+
 	//@ setup the nodes
-	livido_set_portptr_array( filter, "in_parameter_templates",2, in_params );
+	livido_set_portptr_array( filter, "in_parameter_templates",3, in_params );
 	livido_set_portptr_array( filter, "in_channel_templates",0, NULL );
 	livido_set_portptr_array( filter, "out_channel_templates", 1, out_chans );
 
