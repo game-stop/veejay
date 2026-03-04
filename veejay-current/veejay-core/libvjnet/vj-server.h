17abd969d84ddd82282c0749e9d7da5717f2a0b3
niels <>
2026-03-04 22:35:26 +0100

    clean up

--- veejay-current/veejay-core/libvjnet/vj-server.h
+++ veejay-current/veejay-core/libvjnet/vj-server.h
@@ -84,8 +84,6 @@ int	vj_server_link_can_write( vj_server *vje, int link_id );
 
 int	vj_server_link_can_read( vj_server *vje, int link_id);
 
-void	vj_server_geo_stats();
-
 char   *vj_server_my_ip();
 
 #endif
