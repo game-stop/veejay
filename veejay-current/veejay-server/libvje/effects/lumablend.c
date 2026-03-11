8c25209495d4dffa3e6c73bdbff0b678d66be5bf
niels <>
2026-03-12 00:46:52 +0100

    remove the blur in this effect and optimize for speed

--- veejay-current/veejay-server/libvje/effects/lumablend.c
+++ veejay-current/veejay-server/libvje/effects/lumablend.c
@@ -30,7 +30,7 @@ vj_effect *lumablend_init(int w, int h)
 	ve->limits[0] = (int *) vj_calloc(sizeof(int) * ve->num_params);	/* min */
 	ve->limits[1] = (int *) vj_calloc(sizeof(int) * ve->num_params);	/* max */
 	ve->limits[0][0] = 0;	/* type */
-	ve->limits[1][0] = 2;
+	ve->limits[1][0] = 1;
 	ve->limits[0][1] = 0;	/* threshold 1 */
 	ve->limits[1][1] = 255;
 	ve->limits[0][2] = 0;	/* threshold 2 */
@@ -57,157 +57,111 @@ vj_effect *lumablend_init(int w, int h)
 	return ve;
 }
 
-
-static void opacity_by_threshold(VJFrame *frame, VJFrame *frame2,
-								 int threshold, int threshold2, int opacity)
+static inline void opacity_threshold_A(VJFrame * restrict frame,
+                                       VJFrame * restrict frame2,
+                                       int t1, int t2, int opacity)
 {
-	const unsigned int width = frame->width;
-	const int len = frame->len;
-	uint8_t **yuv1 = frame->data;
-	uint8_t **yuv2 = frame2->data;
-	unsigned int x, y;
-	uint8_t a1, a2;
-	unsigned int op0, op1;
-	op1 = (opacity > 255) ? 255 : opacity;
-	op0 = 255 - op1;
+    const int len = frame->len;
 
+    uint8_t * restrict y1 = frame->data[0];
+    uint8_t * restrict u1 = frame->data[1];
+    uint8_t * restrict v1 = frame->data[2];
 
-	for (y = 0; y < len; y += width) {
-		for (x = 0; x < width; x++) {
-			a1 = yuv1[0][x + y];
-			a2 = yuv2[0][x + y];
-			if (a1 >= threshold && a1 <= threshold2) {
-				yuv1[0][x + y] = (op0 * a1 + op1 * a2) >> 8;
-				yuv1[1][x + y] = (op0 * yuv1[1][x + y] + op1 * yuv2[1][x + y]) >> 8;
-				yuv1[2][x + y] = (op0 * yuv1[2][x + y] + op1 * yuv2[2][x + y]) >> 8;
-			}
-		}
-	}
+    uint8_t * restrict y2 = frame2->data[0];
+    uint8_t * restrict u2 = frame2->data[1];
+    uint8_t * restrict v2 = frame2->data[2];
 
-}
+    const unsigned int op1 = (unsigned int)(opacity < 0 ? 0 : (opacity > 255 ? 255 : opacity));
+    const unsigned int op0 = 255u - op1;
 
-static void opacity_by_threshold_(VJFrame *frame, VJFrame *frame2,
-								  int threshold, int threshold2, int opacity)
-{
-	const unsigned int width = frame->width;
-	const int len = frame->len;
-	uint8_t **yuv1 = frame->data;
-	uint8_t **yuv2 = frame2->data;
-	unsigned int x, y;
-	uint8_t a1, a2;
-	unsigned int op0, op1;
-	op1 = (opacity > 255) ? 255 : opacity;
-	op0 = 255 - op1;
-
-	for (y = 0; y < len; y += width) {
-		for (x = 0; x < width; x++) {
-			a1 = yuv1[0][x + y];
-			a2 = yuv2[0][x + y];
-			if (a2 > threshold && a2 < threshold2) {
-				yuv1[0][x + y] = (op0 * a1 + op1 * a2) >> 8;
-				yuv1[1][x + y] = (op0 * yuv1[1][x + y] + op1 * yuv2[1][x + y]) >> 8;
-				yuv1[2][x + y] = (op0 * yuv1[2][x + y] + op1 * yuv2[2][x + y]) >> 8;
-			}
-		}
-	}
-}
-
-static void opacity_by_threshold_blur(VJFrame *frame, VJFrame *frame2,
-									  int threshold, int threshold2, int opacity)
-{
-	const unsigned int width = frame->width;
-	const int len = frame->len - width;
-	uint8_t **yuv1 = frame->data;
-	uint8_t **yuv2 = frame2->data;
-	unsigned int x, y;
-	uint8_t a1=threshold, a2=threshold2;
-	unsigned int op0, op1;
-	op1 = (opacity > 255) ? 255 : opacity;
-	op0 = 255 - op1;
-	for( y = 0; y < width; y+= width ) {
-	  for( x = 0; x < width; x ++ ) {
-		
-			yuv1[0][x + y] = (op0 * a1 + op1 * a2) >> 8;
-			yuv1[1][x + y] =
-			(op0 * yuv1[1][x + y] + op1 * yuv2[1][x + y]) >> 8;
-			yuv1[2][x + y] =
-			(op0 * yuv1[2][x + y] + op1 * yuv2[2][x + y]) >> 8;
-		}
-	}
-	for (y = width; y < len; y += width) {
-			yuv1[0][y] = (op0 * a1 + op1 * a2) >> 8;
-			yuv1[1][y] =
-			(op0 * yuv1[1][y] + op1 * yuv2[1][y]) >> 8;
-			yuv1[2][y] =
-			(op0 * yuv1[2][y] + op1 * yuv2[2][y]) >> 8;
-
-	for (x = 1; x < width-1; x++) {
-		a1 = yuv1[0][x + y];
-		a2 = yuv2[0][x + y];
-		if (a1 >= threshold && a1 <= threshold2) {
-			a1 = (yuv1[0][y - width + x - 1] +
-			  yuv1[0][y - width + x + 1] +
-			  yuv1[0][y - width + x] +
-			  yuv1[0][y + x] +
-			  yuv1[0][y + x - 1] +
-			  yuv1[0][y + x + 1] +
-			  yuv1[0][y + width + x] +
-			  yuv1[0][y + width + x + 1] +
-			  yuv1[0][y + width + x - 1]
-			) / 9;
-
-			a2 = (yuv2[0][y - width + x - 1] +
-			  yuv2[0][y - width + x + 1] +
-			  yuv2[0][y - width + x] +
-			  yuv2[0][y + x] +
-			  yuv2[0][y + x - 1] +
-			  yuv2[0][y + x + 1] +
-			  yuv2[0][y + width + x] +
-			  yuv2[0][y + width + x + 1] +
-			  yuv2[0][y + width + x - 1]
-			) / 9;
-
-			yuv1[0][x + y] = (op0 * a1 + op1 * a2) >> 8;
-			yuv1[1][x + y] =
-			(op0 * yuv1[1][x + y] + op1 * yuv2[1][x + y]) >> 8;
-			yuv1[2][x + y] =
-			(op0 * yuv1[2][x + y] + op1 * yuv2[2][x + y]) >> 8;
-		}
-	   }
-
-	  yuv1[0][width + y] = (op0 * a1 + op1 * a2) >> 8;
-	  yuv1[1][width + y] =
-			(op0 * yuv1[1][width + y] + op1 * yuv2[1][width + y]) >> 8;
-			yuv1[2][width + y] =
-			(op0 * yuv1[2][width + y] + op1 * yuv2[2][width + y]) >> 8;
+    const unsigned int ut1 = (unsigned int)t1;
+    const unsigned int ut2 = (unsigned int)t2;
+    const unsigned int tr = ut2 - ut1;
 	
-   }
-	for( x = 0; x < width; x ++ ) {
-		
-		yuv1[0][x + y] = (op0 * a1 + op1 * a2) >> 8;
-		yuv1[1][x + y] =
-		(op0 * yuv1[1][x + y] + op1 * yuv2[1][x + y]) >> 8;
-		yuv1[2][x + y] =
-		(op0 * yuv1[2][x + y] + op1 * yuv2[2][x + y]) >> 8;
-	}
+    for (int i = 0; i < len; ++i) {
+        const unsigned int a1 = (unsigned int)y1[i];
+        const unsigned int a2 = (unsigned int)y2[i];
 
+        const unsigned int mask = ((a1 - ut1) <= tr) ? 1u : 0u;
+
+        const unsigned int blended_y = (op0 * a1 + op1 * a2) >> 8;
+        const unsigned int ou_y      = a1;
+
+        const unsigned int ou_u      = (unsigned int)u1[i];
+        const unsigned int blended_u = (op0 * ou_u + op1 * (unsigned int)u2[i]) >> 8;
+
+        const unsigned int ou_v      = (unsigned int)v1[i];
+        const unsigned int blended_v = (op0 * ou_v + op1 * (unsigned int)v2[i]) >> 8;
+
+        y1[i] = (uint8_t)( mask * blended_y + (1u - mask) * ou_y );
+        u1[i] = (uint8_t)( mask * blended_u + (1u - mask) * ou_u );
+        v1[i] = (uint8_t)( mask * blended_v + (1u - mask) * ou_v );
+    }
 }
 
-void lumablend_apply(void *ptr, VJFrame *frame, VJFrame *frame2, int *args ) {
-    int type = args[0];
-    int t1 = args[1];
-    int t2 = args[2];
+static inline void opacity_threshold_B(VJFrame * restrict frame,
+                                       VJFrame * restrict frame2,
+                                       int t1, int t2, int opacity)
+{
+    const int len = frame->len;
+
+    uint8_t * restrict y1 = frame->data[0];
+    uint8_t * restrict u1 = frame->data[1];
+    uint8_t * restrict v1 = frame->data[2];
+
+    uint8_t * restrict y2 = frame2->data[0];
+    uint8_t * restrict u2 = frame2->data[1];
+    uint8_t * restrict v2 = frame2->data[2];
+
+    const unsigned int op1 = (unsigned int)(opacity < 0 ? 0 : (opacity > 255 ? 255 : opacity));
+    const unsigned int op0 = 255u - op1;
+
+    const unsigned int ut1 = (unsigned int)t1;
+    const unsigned int ut2 = (unsigned int)t2;
+    const unsigned int tr = ut2 - ut1;
+	
+    for (int i = 0; i < len; ++i) {
+        const unsigned int a1 = (unsigned int)y1[i];
+        const unsigned int a2 = (unsigned int)y2[i];
+
+        const unsigned int mask = ((a2 - ut1) <= tr) ? 1u : 0u;
+
+        const unsigned int blended_y = (op0 * a1 + op1 * a2) >> 8;
+        const unsigned int ou_y      = a1;
+
+        const unsigned int ou_u      = (unsigned int)u1[i];
+        const unsigned int blended_u = (op0 * ou_u + op1 * (unsigned int)u2[i]) >> 8;
+
+        const unsigned int ou_v      = (unsigned int)v1[i];
+        const unsigned int blended_v = (op0 * ou_v + op1 * (unsigned int)v2[i]) >> 8;
+
+        y1[i] = (uint8_t)( mask * blended_y + (1u - mask) * ou_y );
+        u1[i] = (uint8_t)( mask * blended_u + (1u - mask) * ou_u );
+        v1[i] = (uint8_t)( mask * blended_v + (1u - mask) * ou_v );
+    }
+}
+
+void lumablend_apply(void *ptr, VJFrame * frame, VJFrame * frame2, int *args)
+{
+    (void)ptr;
+
+    int type    = args[0];
+    int t1      = args[1];
+    int t2      = args[2];
     int opacity = args[3];
 
-	switch (type) {
-		case 0:
-			opacity_by_threshold(frame, frame2, t1, t2, opacity);
-		break;
-		case 1:
-			opacity_by_threshold_(frame, frame2, t1, t2, opacity);
-		break;
-		case 2:
-			opacity_by_threshold_blur(frame, frame2, t1, t2, opacity);
-		break;
-	}
+    if (t1 > t2) { // invariant
+        int tmp = t1;
+        t1 = t2;
+        t2 = tmp;
+    }
+
+    switch (type) {
+        case 0:
+            opacity_threshold_A(frame, frame2, t1, t2, opacity);
+            break;
+        case 1:
+            opacity_threshold_B(frame, frame2, t1, t2, opacity);
+            break;
+    }
 }
