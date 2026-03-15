3553fceda4a5d962a2d18b1e41b263cf0b99b0c7
niels <>
2026-03-15 02:48:52 +0100

    add mix weight parameter and move pragma omp parallel to outermost block

--- veejay-current/veejay-server/libvje/effects/average-blend.c
+++ veejay-current/veejay-server/libvje/effects/average-blend.c
@@ -22,66 +22,84 @@
 #include <veejaycore/vjmem.h>
 #include "average-blend.h"
 
+typedef struct {
+    int n_threads;
+} avgblend_t;
+
 vj_effect *average_blend_init(int w, int h)
 {
     vj_effect *ve = (vj_effect *) vj_calloc(sizeof(vj_effect));
-    ve->num_params = 1;
-    ve->defaults = (int *) vj_calloc(sizeof(int) * ve->num_params); /* default values */
-    ve->limits[0] = (int *) vj_calloc(sizeof(int) * ve->num_params);    /* min */
-    ve->limits[1] = (int *) vj_calloc(sizeof(int) * ve->num_params);    /* max */
-    ve->limits[0][0] = 0;
-    ve->limits[1][0] = 7;
+    ve->num_params = 2;
+    ve->defaults = (int *) vj_calloc(sizeof(int) * ve->num_params);
+    ve->limits[0] = (int *) vj_calloc(sizeof(int) * ve->num_params);
+    ve->limits[1] = (int *) vj_calloc(sizeof(int) * ve->num_params);
+    
+    ve->limits[0][0] = 1;
+    ve->limits[1][0] = 32;
     ve->defaults[0] = 1;
+    
+    ve->limits[0][1] = 0;
+    ve->limits[1][1] = 255;
+    ve->defaults[1] = 128;
     ve->description = "Average Mixer";
     ve->sub_format = -1;
     ve->extra_frame = 1;
-    ve->parallel = 1;
+    ve->parallel = 0; 
     ve->has_user = 0;
-    ve->param_description = vje_build_param_list( ve->num_params, "Recursions"); 
+    ve->param_description = vje_build_param_list(ve->num_params, "Recursions", "Mix Weight"); 
     return ve;
 }
 
-static inline void ac_average( uint8_t *dst, const uint8_t *src1, const uint8_t *src2, const int len, const int n_threads )
-{
-    unsigned int i;
-#pragma omp parallel for num_threads(n_threads) schedule(static)
-    for( i = 0; i < len; i ++ ) {
-        dst[i] = (src1[i] + src2[i]) >> 1;
-    }
+void *average_blend_malloc(int w, int h) {
+    avgblend_t *t = (avgblend_t*) vj_calloc(sizeof(avgblend_t));
+    t->n_threads = vje_advise_num_threads(w * h);
+    return (void*)t;
 }
 
-static inline void ac_average_uv(uint8_t *dst_u, const uint8_t *src2_u, 
-                                 uint8_t *dst_v, const uint8_t *src2_v, 
-                                 const int len, int n_threads)
-{
-    #pragma omp parallel for num_threads(n_threads) schedule(static)
-    for(int i = 0; i < len; i++) {
-        dst_u[i] = (dst_u[i] + src2_u[i]) >> 1;
-        dst_v[i] = (dst_v[i] + src2_v[i]) >> 1;
-    }
+void average_blend_free(void *ptr) {
+    if(ptr) free(ptr);
 }
 
-static void average_blend_apply1( VJFrame *frame, VJFrame *frame2, int average_blend, const int n_threads)
+void average_blend_apply(void *ptr, VJFrame *frame, VJFrame *frame2, int *args)
 {
-    unsigned int i;
-    for( i = 0; i < average_blend; i ++ ) {
-        ac_average( frame->data[0], frame->data[0],
-            frame2->data[0], frame->len, n_threads );
-        ac_average_uv(frame->data[1], frame2->data[1], 
-                      frame->data[2], frame2->data[2], 
-                      frame->uv_len, n_threads);
+    avgblend_t *t = (avgblend_t*) ptr;
+    const int recursions = args[0];
+    const int weight = args[1];
+    const int n_threads = t->n_threads;
+
+    const int len = frame->len;
+    const int uv_len = frame->uv_len;
+
+    #pragma omp parallel num_threads(n_threads)
+    {
+        for (int r = 0; r < recursions; r++) {
+            
+            uint8_t *restrict Y1 = frame->data[0];
+            const uint8_t *restrict Y2 = frame2->data[0];
+            
+            #pragma omp for schedule(static)
+            for (int i = 0; i < len; i++) {
+                int diff = (int)Y2[i] - (int)Y1[i];
+                Y1[i] = (uint8_t)(Y1[i] + ((weight * diff) >> 8));
+            }
+
+            uint8_t *restrict U1 = frame->data[1];
+            uint8_t *restrict V1 = frame->data[2];
+            const uint8_t *restrict U2 = frame2->data[1];
+            const uint8_t *restrict V2 = frame2->data[2];
+
+            #pragma omp for schedule(static)
+            for (int i = 0; i < uv_len; i++) {
+                int diffU = (int)U2[i] - (int)U1[i];
+                int diffV = (int)V2[i] - (int)V1[i];
+                U1[i] = (uint8_t)(U1[i] + ((weight * diffU) >> 8));
+                V1[i] = (uint8_t)(V1[i] + ((weight * diffV) >> 8));
+            }
+
         }
+    }
 }
 
-void average_blend_apply( void *ptr, VJFrame *frame, VJFrame *frame2, int *args)
-{
-    average_blend_apply1( frame,frame2,args[0], vje_advise_num_threads(frame->len) );
+void average_blend_applyN(void *ptr, VJFrame *frame, VJFrame *frame2, int *args) {
+    average_blend_apply(ptr, frame, frame2, args);
 }
-
-void average_blend_applyN( void *ptr, VJFrame *frame, VJFrame *frame2, int *args) {
-    int average_blend = args[0];
-
-    average_blend_apply1( frame,frame2,average_blend, vje_advise_num_threads(frame->len) );
-    
-}
-
