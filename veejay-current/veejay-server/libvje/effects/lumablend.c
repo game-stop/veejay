af22f374e29f56374c4f6797b9b2b013ad6c3dfe
niels <>
2026-04-05 03:52:46 +0200

    use restrict

--- veejay-current/veejay-server/libvje/effects/lumablend.c
+++ veejay-current/veejay-server/libvje/effects/lumablend.c
@@ -85,8 +85,12 @@ void lumablend_apply(void *ptr, VJFrame * frame, VJFrame * frame2, int *args)
     const int len = frame->len;
     const int t_range = t2 - t1;
 
-    uint8_t *y1 = frame->data[0], *u1 = frame->data[1], *v1 = frame->data[2];
-    uint8_t *y2 = frame2->data[0], *u2 = frame2->data[1], *v2 = frame2->data[2];
+    uint8_t *restrict y1 = frame->data[0];
+    uint8_t *restrict u1 = frame->data[1];
+    uint8_t *restrict v1 = frame->data[2];
+    uint8_t *restrict y2 = frame2->data[0];
+    uint8_t *restrict u2 = frame2->data[1];
+    uint8_t *restrict v2 = frame2->data[2];
 
     const int op1 = opacity;
     const int op0 = 256 - op1;
