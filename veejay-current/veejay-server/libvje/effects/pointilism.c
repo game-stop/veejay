3452eb4925392053856583982cbeb8c3ac9c1afd
niels <>
2026-04-14 00:34:40 +0200

    fix the monday build #333

--- veejay-current/veejay-server/libvje/effects/pointilism.c
+++ veejay-current/veejay-server/libvje/effects/pointilism.c
@@ -18,6 +18,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307 , USA.
  */
 #include <config.h>
+#include <omp.h>
 #include <time.h>
 #include "common.h"
 #include <veejaycore/vjmem.h>
