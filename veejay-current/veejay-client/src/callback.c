ceda6ee2b5999a31d8ee6894bdb0d7df4fc6323c
niels <>
2026-05-31 16:05:00 +0200

    sampe page click should open page 6, not 5 (legacy SRT panel)

--- veejay-current/veejay-client/src/callback.c
+++ veejay-current/veejay-client/src/callback.c
@@ -3966,7 +3966,7 @@ void	on_button_videobook_clicked(GtkWidget *widget, gpointer user_data)
 void	on_samplepage_clicked(GtkWidget *widget, gpointer user_data)
 {
 	GtkWidget *m = glade_xml_get_widget_(info->main_window , "notebook18");
-	gtk_notebook_set_current_page( GTK_NOTEBOOK(m), 5 );
+	gtk_notebook_set_current_page( GTK_NOTEBOOK(m), 6 );
 
 	GtkWidget *n = glade_xml_get_widget_( info->main_window, "panels" );
 
