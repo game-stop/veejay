baf691181ae57cdd646d2801eceab49bdeb54d05
niels <>
2026-08-29 18:51:59 +0200

    remove parallel flag

--- veejay-current/veejay-server/libvje/effects/alphadampen.c
+++ veejay-current/veejay-server/libvje/effects/alphadampen.c
@@ -38,7 +38,7 @@ vj_effect *alphadampen_init(int w, int h)
     ve->sub_format        = -1;
     ve->extra_frame       = 0;
     ve->has_user          = 0;
-    ve->parallel          = 1;
+    ve->parallel          = 0;
     ve->param_description = vje_build_param_list(ve->num_params, "Quantization Step");
     ve->alpha             = FLAG_ALPHA_SRC_A | FLAG_ALPHA_OUT;
 
